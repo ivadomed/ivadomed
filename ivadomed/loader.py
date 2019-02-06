194de8121a4fe8dd63327f3b15914e1690747c9b
Christian S. Perone <christian.perone@gmail.com>
2019-02-06 12:53:07 -0500

    Adding dataloder classes.

---
+++ ivadomed/loader.py
@@ -0,0 +1,65 @@
+from bids_neuropoly import bids
+from medicaltorch import datasets as mt_datasets
+
+
+class BIDSSegPair2D(mt_datasets.SegmentationPair2D):
+    def __init__(self, input_filename, gt_filename, metadata):
+        super().__init__(input_filename, gt_filename)
+        self.metadata = metadata
+
+    def get_pair_slice(self, slice_index, slice_axis=2):
+        dreturn = super().get_pair_slice(slice_index, slice_axis)
+        dreturn["input_metadata"]["bids_metadata"] = self.metadata
+        return dreturn
+
+
+class MRI2DBidsSegDataset(mt_datasets.MRI2DSegmentationDataset):
+    def _load_filenames(self):
+        for input_filename, gt_filename, bids_metadata in self.filename_pairs:
+            segpair = BIDSSegPair2D(input_filename, gt_filename,
+                                    bids_metadata)
+            self.handlers.append(segpair)
+
+
+class BidsDataset(MRI2DBidsSegDataset):
+    def __init__(self, root_dir, slice_axis=2, cache=True,
+                 transform=None, slice_filter_fn=None,
+                 canonical=False, labeled=True):
+        self.bids_ds = bids.BIDS(root_dir)
+        self.filename_pairs = []
+
+        for subject in self.bids_ds.get_subjects():
+            if not subject.has_derivative("labels"):
+                print("Subject without derivative, skipping.")
+                continue
+            derivatives = subject.get_derivatives("labels")
+            cord_label_filename = None
+            for deriv in derivatives:
+                if deriv.endswith("seg-manual.nii.gz"):
+                    cord_label_filename = deriv
+            if cord_label_filename is None:
+                print("Subject without cord label.")
+                continue
+
+            if not subject.has_metadata():
+                print("Subject without metadata.")
+                continue
+
+            metadata = subject.metadata()
+            if "FlipAngle" not in metadata:
+                print("{} without FlipAngle, skipping.".format(subject))
+                continue
+
+            if "EchoTime" not in metadata:
+                print("{} without EchoTime, skipping.".format(subject))
+                continue
+
+            if "RepetitionTime" not in metadata:
+                print("{} without RepetitionTime, skipping.".format(subject))
+                continue
+
+            self.filename_pairs.append((subject.record.absolute_path,
+                                        cord_label_filename, metadata))
+
+        super().__init__(self.filename_pairs, slice_axis, cache,
+                         transform, slice_filter_fn, canonical)
