89d40c475d89e7339b9925c9048cf20d5d58144d
jcohenadad <jcohen@polymtl.ca>
2019-04-03 13:43:35 -0400

    delete_temp_files.sh: Fixed variable names

--- prepare_data/delete_temp_files.sh
+++ prepare_data/delete_temp_files.sh
@@ -3,19 +3,28 @@
 # Delete temporary files. Run this script once all files have been verified.
 
 # Retrieve input params
-sub=$1
-PATH_OUTPUT_wSITE=$2
-PATH_QC=$3
+SUBJECT=$1
+SITE=$2
+PATH_OUTPUT=$3
+PATH_QC=$4
+PATH_LOG=$5
 
 # Create BIDS architecture
-PATH_IN="`pwd`/${sub}/anat"
-ofolder_seg="${PATH_OUTPUT_wSITE}/derivatives/labels/${sub}/anat"
-ofolder_reg="${PATH_OUTPUT_wSITE}/${sub}/anat"
-mkdir -p ${ofolder_reg}
-mkdir -p ${ofolder_seg}
+PATH_OUTPUT_wSITE=${PATH_OUTPUT}/${SITE}
+PATH_IN="`pwd`/${SUBJECT}/anat"
+ofolder_seg="${PATH_OUTPUT_wSITE}/derivatives/labels/${SUBJECT}/anat"
+ofolder_reg="${PATH_OUTPUT_wSITE}/${SUBJECT}/anat"
 
 # Go to output anat folder, where most of the outputs will be located
 cd ${ofolder_reg}
+
+# Set filenames
+file_t1w_mts="${SUBJECT}_acq-T1w_MTS"
+file_mton="${SUBJECT}_acq-MTon_MTS"
+file_mtoff="${SUBJECT}_acq-MToff_MTS"
+file_t2w="${SUBJECT}_T2w"
+file_t2s="${SUBJECT}_T2star"
+file_t1w="${SUBJECT}_T1w"
 
 # Delete temporary files (they interfer with the BIDS wrapper)
 rm *_mask.nii.gz
@@ -24,12 +33,12 @@ rm *_r_reg.*
 rm *crop.*
 rm *mean.*
 rm *T2star_reg.*
-rm ${sub}_acq-T1w_MTS.nii.gz
+rm ${SUBJECT}_acq-T1w_MTS.nii.gz
 rm ${ofolder_seg}/warp*
-rm ${ofolder_seg}/${sub}_T1w_reg_seg.nii.gz
-rm ${ofolder_seg}/${sub}_acq-T1w_MTS_crop_seg_reg.nii.gz
-rm ${ofolder_seg}/${sub}_T2star_reg_seg.nii.gz
-rm ${ofolder_seg}/${sub}_T2w_reg_seg.nii.gz
-rm ${ofolder_seg}/${sub}_*reg.*
-rm ${ofolder_seg}/${sub}_*seg.*
+rm ${ofolder_seg}/${SUBJECT}_T1w_reg_seg.nii.gz
+rm ${ofolder_seg}/${SUBJECT}_acq-T1w_MTS_crop_seg_reg.nii.gz
+rm ${ofolder_seg}/${SUBJECT}_T2star_reg_seg.nii.gz
+rm ${ofolder_seg}/${SUBJECT}_T2w_reg_seg.nii.gz
+rm ${ofolder_seg}/${SUBJECT}_*reg.*
+rm ${ofolder_seg}/${SUBJECT}_*seg.*
 rm ${ofolder_seg}/tmp.*
