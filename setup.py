f6b60030af9c9ecd397e073f603cf12741f889ee
Julien Cohen-Adad <jcohen@polymtl.ca>
2019-04-16 11:40:10 -0400

    setup.py: Added entry points for new package prepdata

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from codecs import open
 from os import path
 
 import ivadomed
+import prepare_data.prepdata as prepdata
 
 here = path.abspath(path.dirname(__file__))
 
@@ -14,7 +15,7 @@ setup(
     version=ivadomed.__version__,
     description='Feature conditioning for IVADO medical imaging project.',
     url='https://github.com/neuropoly/ivado-medical-imaging',
-    author='MILA and NeuroPoly',
+    author='NeuroPoly and Mila',
     author_email='none@none.com',
     classifiers=[
         'Development Status :: 3 - Alpha',
@@ -26,6 +27,27 @@ setup(
     entry_points={
         'console_scripts': [
             'ivadomed=ivadomed.main:run_main',
+        ],
+    },
+)
+
+setup(
+    name='prepdata',
+    version=prepdata.__version__,
+    description='Image manipulation to prepare data before training.',
+    url='https://github.com/neuropoly/ivado-medical-imaging',
+    author='NeuroPoly and Mila',
+    author_email='none@none.com',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+    ],
+    packages=find_packages(exclude=['docs', 'tests']),
+    install_requires=requirements,
+    entry_points={
+        'console_scripts': [
+            'prepdata=prepare_data.prepdata.main:run_main',
         ],
     },
 )
