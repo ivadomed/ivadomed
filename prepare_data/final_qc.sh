0b0a9f9099bdfdfa2dd2885bb7cfecfbdbf50913
Julien Cohen-Adad <jcohen@polymtl.ca>
2019-06-28 16:51:37 -0400

    final_qc.sh: Fixed input, replace rsync with cp
    Clearer when inspecting logs

--- prepare_data/final_qc.sh
+++ prepare_data/final_qc.sh
@@ -9,15 +9,14 @@ set -x
 
 # Retrieve input params
 SUBJECT=$1
-SITE=$2
-PATH_OUTPUT=$3
-PATH_QC=$4
-PATH_LOG=$5
+PATH_OUTPUT=$2
+PATH_QC=$3
+PATH_LOG=$4
 
 # Create BIDS architecture
 PATH_IN="`pwd`/${SUBJECT}/anat"
-ofolder_seg="${PATH_OUTPUT}/${SITE}/derivatives/labels/${SUBJECT}/anat"
-ofolder_reg="${PATH_OUTPUT}/${SITE}/${SUBJECT}/anat"
+ofolder_seg="${PATH_OUTPUT}/derivatives/labels/${SUBJECT}/anat"
+ofolder_reg="${PATH_OUTPUT}/${SUBJECT}/anat"
 
 # Set filenames
 file_t1w_mts="${SUBJECT}_acq-T1w_MTS"
@@ -30,18 +29,10 @@ file_t1w="${SUBJECT}_T1w"
 # Go to output anat folder, where most of the outputs will be located
 cd ${ofolder_reg}
 
-# Set filenames
-file_t1w_mts="${SUBJECT}_acq-T1w_MTS"
-file_mton="${SUBJECT}_acq-MTon_MTS"
-file_mtoff="${SUBJECT}_acq-MToff_MTS"
-file_t2w="${SUBJECT}_T2w"
-file_t2s="${SUBJECT}_T2star"
-file_t1w="${SUBJECT}_T1w"
-
 # Copy files to appropriate locations
-rsync -avzh tmp/${file_t1w_mts}_crop_r.nii.gz ${file_t1w_mts}.nii.gz
-rsync -avzh tmp/${file_mton}_reg.nii.gz ${file_mton}.nii.gz
-rsync -avzh tmp/${file_mtoff}_reg.nii.gz ${file_mtoff}.nii.gz
+cp tmp/${file_t1w_mts}_crop_r.nii.gz ${file_t1w_mts}.nii.gz
+cp tmp/${file_mton}_reg.nii.gz ${file_mton}.nii.gz
+cp tmp/${file_mtoff}_reg.nii.gz ${file_mtoff}.nii.gz
 
 # Rename current files (remove "_reg")
 mv ${file_t1w}_reg.nii.gz ${file_t1w}.nii.gz
@@ -58,7 +49,7 @@ FILES=(
 )
 for file in ${FILES[@]}; do
   # Duplicate segmentation to be used by other contrasts
-  rsync -avzh tmp/${file_t1w_mts}_crop_r_seg-manual.nii.gz ${ofolder_seg}/${file}_seg-manual.nii.gz
+  cp tmp/${file_t1w_mts}_crop_r_seg-manual.nii.gz ${ofolder_seg}/${file}_seg-manual.nii.gz
   # Remove empty slices at the edge
   prepdata -i ${file}.nii.gz -s ${ofolder_seg}/${file}_seg-manual.nii.gz remove-slice
   # Generate final QC
@@ -66,12 +57,12 @@ for file in ${FILES[@]}; do
 done
 
 # Copy json files and rename them
-rsync -avzh ${PATH_IN}/${SUBJECT}_acq-T1w_MTS.json ${file_t1w_mts}.json
-rsync -avzh ${PATH_IN}/${SUBJECT}_acq-MTon_MTS.json ${file_mton}.json
-rsync -avzh ${PATH_IN}/${SUBJECT}_acq-MToff_MTS.json ${file_mtoff}.json
-rsync -avzh ${PATH_IN}/${SUBJECT}_T2w.json ${file_t2w}.json
-rsync -avzh ${PATH_IN}/${SUBJECT}_T2star.json ${file_t2s}.json
-rsync -avzh ${PATH_IN}/${SUBJECT}_T1w.json ${file_t1w}.json
-rsync -avzh ${PATH_IN}/../../dataset_description.json ../../
-rsync -avzh ${PATH_IN}/../../participants.json ../../
-rsync -avzh ${PATH_IN}/../../participants.tsv ../../
+cp ${PATH_IN}/${SUBJECT}_acq-T1w_MTS.json ${file_t1w_mts}.json
+cp ${PATH_IN}/${SUBJECT}_acq-MTon_MTS.json ${file_mton}.json
+cp ${PATH_IN}/${SUBJECT}_acq-MToff_MTS.json ${file_mtoff}.json
+cp ${PATH_IN}/${SUBJECT}_T2w.json ${file_t2w}.json
+cp ${PATH_IN}/${SUBJECT}_T2star.json ${file_t2s}.json
+cp ${PATH_IN}/${SUBJECT}_T1w.json ${file_t1w}.json
+cp ${PATH_IN}/../../dataset_description.json ../../
+cp ${PATH_IN}/../../participants.json ../../
+cp ${PATH_IN}/../../participants.tsv ../../
