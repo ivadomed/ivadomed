2aa9241e33a913efc286f8a6a4620beb69cf5c84
Julien Cohen-Adad <jcohen@polymtl.ca>
2019-06-29 13:48:10 -0400

    _run_with_log.sh: Now displaying TASK name in log file name

--- prepare_data/_run_with_log.sh
+++ prepare_data/_run_with_log.sh
@@ -8,7 +8,9 @@ PATH_QC=$4
 PATH_LOG=$5
 
 cd ${PATH_DATA}
-${TASK} $SUBJECT $PATH_OUTPUT $PATH_QC $PATH_LOG 2>&1 | tee ${PATH_LOG}/${SUBJECT}.log ; test ${PIPESTATUS[0]} -eq 0
+TASK_BASENAME=`basename ${TASK} .sh`
+echo ${TASK_WITHOUT_EXT}
+${TASK} $SUBJECT $PATH_OUTPUT $PATH_QC $PATH_LOG 2>&1 | tee ${PATH_LOG}/${TASK_BASENAME}_${SUBJECT}.log ; test ${PIPESTATUS[0]} -eq 0
 if [ ! $? -eq 0 ]; then
-  mv ${PATH_LOG}/${SUBJECT}.log ${PATH_LOG}/err.${SUBJECT}.log
+  mv ${PATH_LOG}/${TASK_BASENAME}_${SUBJECT}.log ${PATH_LOG}/err.${TASK_BASENAME}_${SUBJECT}.log
 fi
