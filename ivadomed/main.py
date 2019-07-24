dbd45a3c778f9050e6eed74552decfcc01c3ad31
ValentineLL <valentinell@ella.neuro.polymtl.ca>
2019-07-24 10:23:52 -0400

    Change in centercrop: 430 instead of 128

--- ivadomed/main.py
+++ ivadomed/main.py
@@ -62,7 +62,7 @@ class SliceFilter(mt_filters.SliceFilter):
 
         # Filter slices where there are no values after cropping
         input_img = Image.fromarray(sample['input'], mode='F')
-        input_cropped = transforms.functional.center_crop(input_img, (128, 128))
+        input_cropped = transforms.functional.center_crop(input_img, (430, 430))
         input_cropped = np.array(input_cropped)
         count = np.count_nonzero(input_cropped)
 
@@ -111,7 +111,7 @@ def cmd_train(context):
 
     # These are the training transformations
     train_transform = transforms.Compose([
-        mt_transforms.CenterCrop2D((128, 128)),
+        mt_transforms.CenterCrop2D((430, 430)),
         mt_transforms.ElasticTransform(alpha_range=(28.0, 30.0),
                                        sigma_range=(3.5, 4.0),
                                        p=0.3),
@@ -125,7 +125,7 @@ def cmd_train(context):
 
     # These are the validation/testing transformations
     val_transform = transforms.Compose([
-        mt_transforms.CenterCrop2D((128, 128)),
+        mt_transforms.CenterCrop2D((430, 430)),
         mt_transforms.ToTensor(),
         mt_transforms.NormalizeInstance(),
     ])
@@ -414,7 +414,7 @@ def cmd_test(context):
 
     # These are the validation/testing transformations
     val_transform = transforms.Compose([
-        mt_transforms.CenterCrop2D((128, 128)),
+        mt_transforms.CenterCrop2D((430, 430)),
         mt_transforms.ToTensor(),
         mt_transforms.NormalizeInstance(),
     ])
