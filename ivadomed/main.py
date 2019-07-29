c806b480c158cc6b2a120f083cd1f5d616c58758
charleygros <charley.gros@gmail.com>
2019-07-29 07:44:35 -0400

    ivadomed/utils.py: move mixup from main.py to utils.py

--- ivadomed/main.py
+++ ivadomed/main.py
@@ -25,86 +25,11 @@ from tqdm import tqdm
 
 from ivadomed import loader as loader
 from ivadomed import models
+from ivadomed.utils import *
 
 import numpy as np
-import matplotlib.pyplot as plt
-
-from PIL import Image
 
 cudnn.benchmark = True
-
-
-def mixup(data, targets, alpha):
-    """Compute the mixup data.
-    Return mixed inputs and targets, lambda.
-    """
-    indices = torch.randperm(data.size(0))
-    data2 = data[indices]
-    targets2 = targets[indices]
-
-    lambda_ = np.random.beta(alpha, alpha)
-    lambda_ = max(lambda_, 1 - lambda_) # ensure lambda_ >= 0.5
-    lambda_tensor = torch.FloatTensor([lambda_])
-
-    data = data * lambda_tensor + data2 * (1 - lambda_tensor)
-    targets = targets * lambda_tensor + targets2 * (1 - lambda_tensor)
-
-    return data, targets, lambda_tensor
-
-
-def save_mixup_sample(x, y, fname):
-
-    plt.figure(figsize=(20, 10))
-    plt.subplot(1, 2, 1)
-    plt.axis("off")
-    plt.imshow(x, interpolation='nearest', aspect='auto', cmap='gray')
-
-    plt.subplot(1, 2, 2)
-    plt.axis("off")
-    plt.imshow(y, interpolation='nearest', aspect='auto', cmap='jet', vmin=0, vmax=1)
-
-    plt.savefig(fname, bbox_inches='tight', pad_inches=0, dpi=100)
-    plt.close()
-
-
-def threshold_predictions(predictions, thr=0.5):
-    """This function will threshold predictions.
-
-    :param predictions: input data (predictions)
-    :param thr: threshold to use, default to 0.5
-    :return: thresholded input
-    """
-    thresholded_preds = predictions[:]
-    low_values_indices = thresholded_preds < thr
-    thresholded_preds[low_values_indices] = 0
-    low_values_indices = thresholded_preds >= thr
-    thresholded_preds[low_values_indices] = 1
-    return thresholded_preds
-
-
-class SliceFilter(mt_filters.SliceFilter):
-    """This class extends the SliceFilter that already
-    filters for empty labels and inputs. It will filter
-    slices that has only zeros after cropping. To avoid
-    feeding empty inputs into the network.
-    """
-    def __call__(self, sample):
-        super_ret = super().__call__(sample)
-
-        # Already filtered by base class
-        if not super_ret:
-            return super_ret
-
-        # Filter slices where there are no values after cropping
-        input_img = Image.fromarray(sample['input'], mode='F')
-        input_cropped = transforms.functional.center_crop(input_img, (128, 128))
-        input_cropped = np.array(input_cropped)
-        count = np.count_nonzero(input_cropped)
-
-        if count <= 0:
-            return False
-
-        return True
 
 
 def cmd_train(context):
@@ -228,7 +153,7 @@ def cmd_train(context):
     scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, num_epochs)
 
     # Write the metrics, images, etc to TensorBoard format
-    writer = SummaryWriter(log_dir=context["log_directory"])
+    writer = SummaryWriter(logdir=context["log_directory"])
 
     # Create dict containing gammas and betas after each FiLM layer.
     gammas_dict = {i:[] for i in range(1,9)}
