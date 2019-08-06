2c3fbe24044dd65214b53f612227f5618eb4be8f
charleygros <charley.gros@gmail.com>
2019-08-05 20:18:26 -0400

    dev/seek_contrast_sctTesting.py: init

---
+++ dev/seek_contrast_sctTesting.py
@@ -0,0 +1,18 @@
+# Usage:
+#	python dev/seek_contrast_sctTesting.py
+# Example:
+#	python dev/seek_contrast_sctTesting.py
+
+import sys
+import os
+import json
+import numpy as np
+from tqdm import tqdm
+
+PATH_SCTTESTING = '~/duke/sct_testing/large/'
+
+def run_main():
+    pass
+
+if __name__ == "__main__":
+    run_main()
