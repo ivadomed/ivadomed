25b8623730b47b58f7f02de7a19df34a01338eab
charleygros <charley.gros@gmail.com>
2019-11-20 10:12:27 -0500

    testing/test_sampler.py: comparison

--- testing/test_sampler.py
+++ testing/test_sampler.py
@@ -35,7 +35,7 @@ def test_sampler():
                                   subject_lst=train_lst,
                                   target_suffix="_lesion-manual",
                                   roi_suffix="_seg-manual",
-                                  contrast_lst=['acq-ax_T2w'],
+                                  contrast_lst=['acq-sagstir_T2w'], #['acq-ax_T2w'],
                                   metadata_choice="without",
                                   contrast_balance={},
                                   slice_axis=2,
@@ -43,11 +43,35 @@ def test_sampler():
                                   multichannel=False,
                                   slice_filter_fn=SliceFilter(nb_nonzero_thr=10))
 
-    loader.BalancedSampler(ds_train)
-
     print(f"Loaded {len(ds_train)} axial slices for the training set.")
-    train_loader = DataLoader(ds_train, batch_size=context["batch_size"],
+    train_loader = DataLoader(ds_train, batch_size=8,
                               shuffle=True, pin_memory=True,
                               collate_fn=mt_datasets.mt_collate,
                               num_workers=0)
+
+    cmpt_label = {0: 0, 1: 0}
+    for i, batch in enumerate(train_loader):
+        gt_samples = batch["gt"]
+        for idx in range(len(gt_samples)):
+            if np.any(gt_samples[idx]):
+                cmpt_label[1] += 1
+            else:
+                cmpt_label[0] += 1
+    print(cmpt_label)
+
+    train_loader_balanced = DataLoader(ds_train, batch_size=8,
+                                          sampler=loader.BalancedSampler(ds_train),
+                                          shuffle=False, pin_memory=True,
+                                          collate_fn=mt_datasets.mt_collate,
+                                          num_workers=0)
+
+    cmpt_label = {0: 0, 1: 0}
+    for i, batch in enumerate(train_loader_balanced):
+        gt_samples = batch["gt"]
+        for idx in range(len(gt_samples)):
+            if np.any(gt_samples[idx]):
+                cmpt_label[1] += 1
+            else:
+                cmpt_label[0] += 1
+    print(cmpt_label)
 
