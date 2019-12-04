40ad95646aa9cea40155659c846811bd93965e5e
charleygros <charley.gros@gmail.com>
2019-12-04 04:40:02 -0500

    testing/test_training_time.py: add timing for each step

--- testing/test_training_time.py
+++ testing/test_training_time.py
@@ -1,5 +1,6 @@
 import numpy as np
 import time
+import sys
 
 import torch.backends.cudnn as cudnn
 from torch.utils.data import DataLoader
@@ -69,7 +70,7 @@ def test_film_contrast(film_layers=FILM_LAYERS):
     train_loader = DataLoader(ds_train, batch_size=BATCH_SIZE,
                               shuffle=True, pin_memory=True,
                               collate_fn=mt_datasets.mt_collate,
-                              num_workers=0)
+                              num_workers=1)
 
     model = models.FiLMedUnet(n_metadata=len([ll for l in train_onehotencoder.categories_ for ll in l]),
                                film_bool=film_layers,
@@ -82,14 +83,23 @@ def test_film_contrast(film_layers=FILM_LAYERS):
     optimizer = optim.Adam(model.parameters(), lr=INIT_LR)
     scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, N_EPOCHS)
 
+    load_lst, pred_lst, opt_lst, schedul_lst, init_lst, gen_lst = [], [], [], [], [], []
     for epoch in tqdm(range(1, N_EPOCHS + 1), desc="Training"):
         start_time = time.time()
 
+        start_init = time.time()
         lr = scheduler.get_lr()[0]
-
         model.train()
+        tot_init = time.time() - start_init
+        init_lst.append(tot_init)
+
         num_steps = 0
         for i, batch in enumerate(train_loader):
+            if i > 0:
+                tot_gen = time.time() - start_gen
+                gen_lst.append(tot_gen)
+
+            start_load = time.time()
             input_samples, gt_samples = batch["input"], batch["gt"]
             if cuda_available:
                 var_input = input_samples.cuda()
@@ -100,8 +110,15 @@ def test_film_contrast(film_layers=FILM_LAYERS):
 
             sample_metadata = batch["input_metadata"]
             var_metadata = [train_onehotencoder.transform([sample_metadata[k]['film_input']]).tolist()[0] for k in range(len(sample_metadata))]
-            preds = model(var_input, var_metadata)  # Input the metadata related to the input samples
+            tot_load = time.time() - start_load
+            load_lst.append(tot_load)
 
+            start_pred = time.time()
+            preds = model(var_input, var_metadata)  # Input the metadata related to the input samples
+            tot_pred = time.time() - start_pred
+            pred_lst.append(tot_pred)
+
+            start_opt = time.time()
             loss = - losses.dice_loss(preds, var_gt)
 
             optimizer.zero_grad()
@@ -112,13 +129,28 @@ def test_film_contrast(film_layers=FILM_LAYERS):
                 scheduler.step()
 
             num_steps += 1
+            tot_opt = time.time() - start_opt
+            opt_lst.append(tot_opt)
 
+            start_gen = time.time()
+
+        start_schedul = time.time()
         if not step_scheduler_batch:
             scheduler.step()
+        tot_schedul = time.time() - start_schedul
+        schedul_lst.append(tot_schedul)
 
         end_time = time.time()
         total_time = end_time - start_time
         tqdm.write("Epoch {} took {:.2f} seconds.".format(epoch, total_time))
+
+    print('Mean SD init {} -- {}'.format(np.mean(init_lst), np.std(init_lst)))
+    print('Mean SD load {} -- {}'.format(np.mean(load_lst), np.std(load_lst)))
+    print('Mean SD pred {} -- {}'.format(np.mean(pred_lst), np.std(pred_lst)))
+    print('Mean SDopt {} --  {}'.format(np.mean(opt_lst), np.std(opt_lst)))
+    print('Mean SD gen {} -- {}'.format(np.mean(gen_lst), np.std(gen_lst)))
+    print('Mean SD scheduler {} -- {}'.format(np.mean(schedul_lst), np.std(schedul_lst)))
+
 
 def test_unet():
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -153,7 +185,7 @@ def test_unet():
     train_loader = DataLoader(ds_train, batch_size=BATCH_SIZE,
                               shuffle=True, pin_memory=True,
                               collate_fn=mt_datasets.mt_collate,
-                              num_workers=0)
+                              num_workers=1)
 
     model = models.Unet(
                         in_channel=1,
@@ -161,21 +193,29 @@ def test_unet():
                         depth=2,
                         drop_rate=DROPOUT,
                         bn_momentum=BN)
-    if torch.cuda.is_available():
+    if cuda_available:
         model.cuda()
 
     step_scheduler_batch = False
     optimizer = optim.Adam(model.parameters(), lr=INIT_LR)
     scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, N_EPOCHS)
 
+    load_lst, pred_lst, opt_lst, schedul_lst, init_lst, gen_lst = [], [], [], [], [], []
     for epoch in tqdm(range(1, N_EPOCHS + 1), desc="Training"):
         start_time = time.time()
 
+        start_init = time.time()
         lr = scheduler.get_lr()[0]
-
         model.train()
+        tot_init = time.time() - start_init
+        init_lst.append(tot_init)
+
         num_steps = 0
         for i, batch in enumerate(train_loader):
+            if i > 0:
+                tot_gen = time.time() - start_gen
+                gen_lst.append(tot_gen)
+            start_load = time.time()
             input_samples, gt_samples = batch["input"], batch["gt"]
             if cuda_available:
                 var_input = input_samples.cuda()
@@ -183,9 +223,15 @@ def test_unet():
             else:
                 var_input = input_samples
                 var_gt = gt_samples
+            tot_load = time.time() - start_load
+            load_lst.append(tot_load)
 
+            start_pred = time.time()
             preds = model(var_input)
+            tot_pred = time.time() - start_pred
+            pred_lst.append(tot_pred)
 
+            start_opt = time.time()
             loss = - losses.dice_loss(preds, var_gt)
 
             optimizer.zero_grad()
@@ -196,10 +242,25 @@ def test_unet():
                 scheduler.step()
 
             num_steps += 1
+            tot_opt = time.time() - start_opt
+            opt_lst.append(tot_opt)
 
+            start_gen = time.time()
+
+        start_schedul = time.time()
         if not step_scheduler_batch:
             scheduler.step()
+        tot_schedul = time.time() - start_schedul
+        schedul_lst.append(tot_schedul)
 
         end_time = time.time()
         total_time = end_time - start_time
         tqdm.write("Epoch {} took {:.2f} seconds.".format(epoch, total_time))
+
+    print('Mean SD init {} -- {}'.format(np.mean(init_lst), np.std(init_lst)))
+    print('Mean SD load {} -- {}'.format(np.mean(load_lst), np.std(load_lst)))
+    print('Mean SD pred {} -- {}'.format(np.mean(pred_lst), np.std(pred_lst)))
+    print('Mean SD opt {} --  {}'.format(np.mean(opt_lst), np.std(opt_lst)))
+    print('Mean SD gen {} -- {}'.format(np.mean(gen_lst), np.std(gen_lst)))
+    print('Mean SD scheduler {} -- {}'.format(np.mean(schedul_lst), np.std(schedul_lst)))
+
