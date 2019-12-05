49c9d12f23be1b2f52c5d5ea3c87d4aca1eec248
olivier <oli_ch@hotmail.com>
2019-12-05 11:19:51 -0500

    Start method not needed for thread

--- dev/automate_training.py
+++ dev/automate_training.py
@@ -27,10 +27,12 @@ logging.basicConfig(filename=LOG_FILENAME, level=logging.DEBUG)
 
 gpus_available = mp.Queue()
 
+
 def get_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument("-c", "--config", required=True, help="Base config file path.")
-    parser.add_argument("--all-combin", dest='all_combin', action='store_true', help="To run all combinations of config")
+    parser.add_argument("--all-combin", dest='all_combin', action='store_true',
+                        help="To run all combinations of config")
     parser.set_defaults(all_combin=False)
 
     return parser
@@ -38,20 +40,21 @@ def get_parser():
 
 def worker(config):
 
-    #Use GPU i from the array specified in the config file
+    # Use GPU i from the array specified in the config file
     config["gpu"] = gpus_available.get()
 
-    #Call ivado cmd_train
+    # Call ivado cmd_train
     try:
-        #Save best validation score
-        best_training_dice, best_training_loss, best_validation_dice, best_validation_loss = ivado.cmd_train(config)
+        # Save best validation score
+        best_training_dice, best_training_loss, best_validation_dice, best_validation_loss = ivado.cmd_train(
+            config)
     except:
         logging.exception('Got exception on main handler')
         print("Unexpected error:", sys.exc_info()[0])
         raise
 
     # Save config file in log directory
-    config_copy = open(config["log_directory"] + "/config.json","w")
+    config_copy = open(config["log_directory"] + "/config.json", "w")
     json.dump(config, config_copy, indent=4)
 
     gpus_available.put(config["gpu"])
@@ -63,13 +66,13 @@ if __name__ == '__main__':
     parser = get_parser()
     args = parser.parse_args()
 
-    #Load initial config
+    # Load initial config
     with open(args.config, "r") as fhandle:
         initial_config = json.load(fhandle)
 
-    ### Hyperparameters values to test
+    # Hyperparameters values to test
 
-    ## Step 1 : batch size, initial LR and LR scheduler
+    # Step 1 : batch size, initial LR and LR scheduler
 
     batch_sizes = [8, 16, 32, 64]
     initial_lrs = [1e-2, 1e-3, 1e-4, 1e-5]
@@ -80,11 +83,11 @@ if __name__ == '__main__':
                     {"name": "CyclicLR", "base_lr" : X, "max_lr" : Y}]
     """
 
-    ## Step 2 : Losses (dice, cross_entropy, focal, mixed, gdl)
+    # Step 2 : Losses (dice, cross_entropy, focal, mixed, gdl)
 
     #losses = [{"name": "dice"}, {"name": "cross_entropy"}, {"name": "gdl"}]
 
-    #Focal loss
+    # Focal loss
     """
     base_loss = {"name": "focal", "params": {"gamma": 0.5, "alpha" : 0.2}}
     alphas = [0.2, 0.5, 0.75, 1]
@@ -109,34 +112,32 @@ if __name__ == '__main__':
     #print(losses)
     """
 
-    ## Step 3 : FiLM
+    # Step 3 : FiLM
 
     #metadatas = ["contrast"]
 
-    #film_layers = [ [1, 0, 0, 0, 0, 0, 0, 0],
+    # film_layers = [ [1, 0, 0, 0, 0, 0, 0, 0],
     #                [0, 0, 0, 0, 1, 0, 0, 0],
     #                [0, 0, 0, 0, 0, 0, 0, 1],
     #                [1, 1, 1, 1, 1, 1, 1, 1]]
 
-    ## Step 4 : Mixup
+    # Step 4 : Mixup
 
     #mixup_bools = [True]
     #mixup_alphas = [0.5, 1, 2]
 
-    ## Step 5 : Dilation
+    # Step 5 : Dilation
 
     #gt_dilations = [0, 0.5, 1]
-
-
 
     # Split dataset if not already done
     if initial_config.get("split_path") is None:
         train_lst, valid_lst, test_lst = loader.split_dataset(path_folder=initial_config["bids_path"],
-                                                      center_test_lst=initial_config["center_test"],
-                                                      split_method=initial_config["split_method"],
-                                                      random_seed=initial_config["random_seed"],
-                                                      train_frac=initial_config["train_fraction"],
-                                                      test_frac=initial_config["test_fraction"])
+                                                              center_test_lst=initial_config["center_test"],
+                                                              split_method=initial_config["split_method"],
+                                                              random_seed=initial_config["random_seed"],
+                                                              train_frac=initial_config["train_fraction"],
+                                                              test_frac=initial_config["test_fraction"])
 
         # save the subject distribution
         split_dct = {'train': train_lst, 'valid': valid_lst, 'test': test_lst}
@@ -144,16 +145,16 @@ if __name__ == '__main__':
         joblib.dump(split_dct, split_path)
         initial_config["split_path"] = split_path
 
-
-    #Dict with key corresponding to name of the param in the config file
-    param_dict = {"batch_size":batch_sizes, "initial_lr":initial_lrs}
+    # Dict with key corresponding to name of the param in the config file
+    param_dict = {"batch_size": batch_sizes, "initial_lr": initial_lrs}
 
     config_list = []
-    #Test all combinations (change multiple parameters for each test)
+    # Test all combinations (change multiple parameters for each test)
     if args.all_combin:
 
-        #Cartesian product (all combinations)
-        combinations = (dict(zip(param_dict.keys(), values)) for values in product(*param_dict.values()))
+        # Cartesian product (all combinations)
+        combinations = (dict(zip(param_dict.keys(), values))
+                        for values in product(*param_dict.values()))
 
         for combination in combinations:
 
@@ -163,10 +164,11 @@ if __name__ == '__main__':
 
                 value = combination[param]
                 new_config[param] = value
-                new_config["log_directory"] = new_config["log_directory"] + "-" + param + "=" + str(value)
+                new_config["log_directory"] = new_config["log_directory"] + \
+                    "-" + param + "=" + str(value)
 
             config_list.append(copy.deepcopy(new_config))
-    #Change a single parameter for each test
+    # Change a single parameter for each test
     else:
         for param in param_dict:
 
@@ -175,33 +177,25 @@ if __name__ == '__main__':
             for value in param_dict[param]:
 
                 new_config[param] = value
-                new_config["log_directory"] = initial_config["log_directory"] + "-" + param + "=" + str(value)
+                new_config["log_directory"] = initial_config["log_directory"] + \
+                    "-" + param + "=" + str(value)
                 config_list.append(copy.deepcopy(new_config))
-
-
-
-
-
-    #CUDA problem when forking process
-    #https://github.com/pytorch/pytorch/issues/2517
-    mp.set_start_method('spawn')
-
-    #Run all configs on a separate process, with a maximum of n_gpus  processes at a given time
 
     for gpu in initial_config["gpu"]:
         gpus_available.put(gpu)
 
-    pool = mp.pool.ThreadPool(processes = len(initial_config["gpu"]))
-    validation_scores = pool.map(worker,config_list)
+    # Run all configs on a separate process, with a maximum of n_gpus  processes at a given time
+    pool = mp.pool.ThreadPool(processes=len(initial_config["gpu"]))
+    validation_scores = pool.map(worker, config_list)
 
-
-    #Merge config and results in a df
+    # Merge config and results in a df
     config_df = pd.DataFrame.from_dict(config_list)
     keep = list(param_dict.keys())
     keep.append("log_directory")
     config_df = config_df[keep]
 
-    results_df = pd.DataFrame(validation_scores, columns =['log_directory', 'best_training_dice','best_training_loss', 'best_validation_dice', 'best_validation_loss'])
+    results_df = pd.DataFrame(validation_scores, columns=[
+                              'log_directory', 'best_training_dice', 'best_training_loss', 'best_validation_dice', 'best_validation_loss'])
     results_df = config_df.set_index('log_directory').join(results_df.set_index('log_directory'))
     results_df = results_df.sort_values(by=['best_validation_loss'])
 
