1fd5aebaec1178c163f37ebb08c10364bcb20516
charleygros <charley.gros@gmail.com>
2019-12-09 04:09:18 -0500

    testing/test_dilateGT.py: save_im_gt

--- testing/test_dilateGT.py
+++ testing/test_dilateGT.py
@@ -1,6 +1,7 @@
 import numpy as np
 import time
 import sys
+import matplotlib.pyplot as plt
 
 import torch.backends.cudnn as cudnn
 from torch.utils.data import DataLoader
@@ -28,14 +29,34 @@ BN = 0.1
 N_EPOCHS = 10
 INIT_LR = 0.01
 PATH_BIDS = 'testing_data/'
+PATH_TMP = 'tmp_test_dilateGT/'
+
+
+def save_im_gt(im, gt, fname_out):
+    plt.figure()
+    plt.subplot(1, 2, 1)
+    plt.axis("off")
+
+    i_zero, i_nonzero = np.where(gt==0.0), np.nonzero(gt)
+    img_jet = plt.cm.jet(plt.Normalize(vmin=0, vmax=1)(gt))
+    img_jet[i_zero] = 0.0
+    bkg_grey = plt.cm.binary_r(plt.Normalize(vmin=np.amin(im), vmax=np.amax(im))(im))
+    img_out = np.copy(bkg_grey)
+    img_out[i_nonzero] = img_jet[i_nonzero]
+
+    plt.subplot(1, 2, 1)
+    plt.axis("off")
+    plt.imshow(bkg_grey, interpolation='nearest', aspect='auto')
+
+    plt.subplot(1, 2, 2)
+    plt.axis("off")
+    plt.imshow(img_out, interpolation='nearest', aspect='auto')
+
+    plt.savefig(fname_out, bbox_inches='tight', pad_inches=0)
+    plt.close()
+
 
 def test_dilateGT():
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    cuda_available = torch.cuda.is_available()
-    if cuda_available:
-        torch.cuda.set_device(GPU_NUMBER)
-        print("Using GPU number {}".format(GPU_NUMBER))
-
     training_transform_list = [
         ivadomed_transforms.Resample(wspace=0.75, hspace=0.75),
         ivadomed_transforms.DilateGT(dilation_factor=2),
@@ -65,9 +86,11 @@ def test_dilateGT():
                               collate_fn=mt_datasets.mt_collate,
                               num_workers=1)
 
-    if cuda_available:
-        model.cuda()
+    if not os.path.isdir(PATH_OUT):
+        os.makedirs(PATH_OUT)
 
     for i, batch in enumerate(train_loader):
         input_samples, gt_samples = batch["input"], batch["gt"]
-
+        for b_idx in range(len(batch['input'])):
+            fname_out = os.path.join(PATH_OUT, 'im_'+str(i).zfill(2)+'_'+str(b_idx).zfill(2)+'.png')
+            save_im_gt(input_samples[b_idx], gt_samples[b_idx], fname_out)
