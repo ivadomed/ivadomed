d3495930852c1f8581dceee87042f5ecf9f801db
charleygros <charley.gros@gmail.com>
2019-12-11 21:49:34 -0500

    testing/test_undoTrans.py: resample ok

--- testing/test_undoTrans.py
+++ testing/test_undoTrans.py
@@ -1,6 +1,7 @@
 import os
 import numpy as np
 import time
+from random import randint
 
 import torch.backends.cudnn as cudnn
 from torch.utils.data import DataLoader, dataloader
@@ -28,7 +29,7 @@ SLICE_AXIS = 2
 PATH_BIDS = 'testing_data/'
 PATH_OUT = 'tmp/'
 
-def test_undo(contrast='T2w'):
+def test_undo(contrast='T2star'):
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     cuda_available = torch.cuda.is_available()
     if not cuda_available:
@@ -40,8 +41,8 @@ def test_undo(contrast='T2w'):
         print("using GPU number {}".format(GPU_NUMBER))
 
     validation_transform_list = [
-        #ivadomed_transforms.Resample(wspace=0.75, hspace=0.75),
-        ivadomed_transforms.CenterCrop2D(size=[40, 48]),
+        ivadomed_transforms.Resample(wspace=0.75, hspace=0.75),
+        #ivadomed_transforms.CenterCrop2D(size=[40, 48]),
         #ivadomed_transforms.ROICrop2D(size=[40, 48]),
         ivadomed_transforms.ToTensor(),
         ivadomed_transforms.NormalizeInstance()
@@ -70,7 +71,7 @@ def test_undo(contrast='T2w'):
                                   multichannel=False,
                                   slice_filter_fn=SliceFilter(filter_empty_input=True,
                                                                 filter_empty_mask=False))
-    ds_test.filter_roi(nb_nonzero_thr=10)
+    #ds_test.filter_roi(nb_nonzero_thr=10)
 
     ds_test_noTrans = loader.BidsDataset(PATH_BIDS,
                                   subject_lst=test_lst,
@@ -84,7 +85,7 @@ def test_undo(contrast='T2w'):
                                   multichannel=False,
                                   slice_filter_fn=SliceFilter(filter_empty_input=True,
                                                                 filter_empty_mask=False))
-    ds_test_noTrans.filter_roi(nb_nonzero_thr=10)
+    #ds_test_noTrans.filter_roi(nb_nonzero_thr=10)
 
     test_loader = DataLoader(ds_test, batch_size=len(ds_test),
                              shuffle=False, pin_memory=True,
@@ -109,5 +110,19 @@ def test_undo(contrast='T2w'):
         rdict_undo = val_undo_transform(rdict)
 
         before, after = np.array(gt_undone[smp_idx][0]), np.array(rdict_undo['gt'])
-        print(before.shape, after.shape, np.sum(before), np.sum(after))
-        print(np.array_equal(before, after))
+        after[after > 0] = 1.0
+        print(before.shape == after.shape, np.sum(before) == np.sum(after), np.sum(before-after) == 0.0)
+        print(before.shape, after.shape, np.sum(before), np.sum(after), np.sum(before-after), np.unique(before), np.unique(after))
+
+        #print(np.array_equal(before, after))
+        # print((before == after).all())
+        # print(np.allclose(before, after))
+        # print(np.unique(before), np.unique(after))
+        if 1 in before:
+            fig = plt.figure(figsize=(20,10))
+            ax1 = fig.add_subplot(1,2,1)
+            im = ax1.imshow(np.array(before), cmap='gray')
+            ax2 = fig.add_subplot(1,2,2)
+            im = ax2.imshow(np.array(after), cmap='gray')
+            plt.savefig('tmpT/'+str(randint(0,1000))+'.png')
+            plt.close()
