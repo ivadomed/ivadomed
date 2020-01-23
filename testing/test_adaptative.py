486eb740ba589da346e06e7d8d8b7cb8d76188f9
AnBucquet <bucquet@eurecom.fr>
2020-01-23 14:01:39 -0500

    typo.

--- testing/test_adaptative.py
+++ testing/test_adaptative.py
@@ -14,7 +14,7 @@ PATH_BIDS = 'testing_data'
 
 
 def test_hdf5():
-    print('[INFO]: Starting test ... ')
+    print('[INFO]: Starting test ... \n')
     train_lst = ['sub-test001']
 
     hdf5_file = adaptative.Bids_to_hdf5(PATH_BIDS,
@@ -30,14 +30,14 @@ def test_hdf5():
 
     # Checking architecture
     def print_attrs(name, obj):
-        print("Name of the object: {}".format(name))
-        print("Type: {}".format(type(name)))
+        print("\nName of the object: {}".format(name))
+        print("Type: {}".format(type(obj)))
         print("Including the following attributes:")
         for key, val in obj.attrs.items():
             print("    %s: %s" % (key, val))
 
     hdf5_file.hdf5_file.visititems(print_attrs)
-    print('[INFO]: Test passed successfully. ')
+    print('\n[INFO]: Test passed successfully. ')
 
 
 test_hdf5()
