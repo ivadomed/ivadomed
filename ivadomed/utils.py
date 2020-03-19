d8795d150cac111a58ef24e7fe12e61e7222dd07
olivier <oli_ch@hotmail.com>
2020-03-19 13:18:50 -0400

    Move SliceFilter to utils.py

--- ivadomed/utils.py
+++ ivadomed/utils.py
@@ -880,3 +880,22 @@ def save_feature_map(batch, layer_name, context, model, test_input, slice_axis):
         nib_pred = nib.Nifti1Image(attention_map, nib_ref.affine)
 
         nib.save(nib_pred, save_directory)
+
+class SliceFilter(object):
+    def __init__(self, filter_empty_mask=True,
+                 filter_empty_input=True):
+        self.filter_empty_mask = filter_empty_mask
+        self.filter_empty_input = filter_empty_input
+
+    def __call__(self, sample):
+        input_data, gt_data = sample['input'], sample['gt']
+
+        if self.filter_empty_mask:
+            if not np.any(gt_data):
+                return False
+
+        if self.filter_empty_input:
+            if not np.all([np.any(img) for img in input_data]):
+                return False
+
+        return True
