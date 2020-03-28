27fc1d09466fa11f8637ac10966299a72a79a33d
andreanne-lemay <andreanne.lemay@polymtl.ca>
2020-03-28 16:02:41 -0400

    correct dice score and losses

--- ivadomed/utils.py
+++ ivadomed/utils.py
@@ -594,7 +594,7 @@ def multiclass_dice_score(im1, im2):
         dice_per_class += dice_score(im1[:, i, ], im2[:, i, ]) \
             if not dice_score(im1[:, i, ], im2[:, i, ]) == np.nan else 1
 
-    return (2.0 * dice_per_class) / n_classes
+    return  dice_per_class / n_classes
 
 
 def dice_score(im1, im2):
