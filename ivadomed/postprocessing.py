4b12728af8bd3ae70a817147fba7d347e6e244e1
charleygros <charley.gros@gmail.com>
2020-04-05 21:55:34 -0400

    ivadomed/utils.py: move thresholding fct from utils to pp

--- ivadomed/postprocessing.py
+++ ivadomed/postprocessing.py
@@ -2,3 +2,17 @@
 
 import numpy as np
 from scipy.ndimage.measurements import label
+
+
+def threshold_predictions(predictions, thr=0.5):
+    """This function will threshold predictions.
+    :param predictions: input data (predictions)
+    :param thr: threshold to use, default to 0.5
+    :return: thresholded input
+    """
+    thresholded_preds = predictions[:]
+    low_values_indices = thresholded_preds < thr
+    thresholded_preds[low_values_indices] = 0
+    low_values_indices = thresholded_preds >= thr
+    thresholded_preds[low_values_indices] = 1
+    return thresholded_preds
--- ivadomed/utils.py
+++ ivadomed/utils.py
@@ -567,20 +567,6 @@ def save_mixup_sample(x, y, fname):
     plt.close()
 
 
-def threshold_predictions(predictions, thr=0.5):
-    """This function will threshold predictions.
-    :param predictions: input data (predictions)
-    :param thr: threshold to use, default to 0.5
-    :return: thresholded input
-    """
-    thresholded_preds = predictions[:]
-    low_values_indices = thresholded_preds < thr
-    thresholded_preds[low_values_indices] = 0
-    low_values_indices = thresholded_preds >= thr
-    thresholded_preds[low_values_indices] = 1
-    return thresholded_preds
-
-
 def segment_volume(folder_model, fname_image, fname_roi=None):
     """Segment an image.
 
