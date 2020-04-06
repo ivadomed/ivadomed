ac40f09312b90b332ad932184c93596562c4e879
charleygros <charley.gros@gmail.com>
2020-04-05 22:19:50 -0400

    ivadomed/postprocessing.py: keep_largest_object

--- ivadomed/postprocessing.py
+++ ivadomed/postprocessing.py
@@ -24,3 +24,21 @@ def threshold_predictions(predictions, thr=0.5):
     low_values_indices = thresholded_preds >= thr
     thresholded_preds[low_values_indices] = 1
     return thresholded_preds
+
+
+def keep_largest_object(predictions):
+    """Keep the largest connect object.
+
+    Keep the largest connected object per z_slice and fill little holes.
+    Note: This function only works for binary segmentation.
+    :param z_slice: int 2d-array: Input 2d segmentation
+    :return: z_slice: int 2d-array: Processed 2d segmentation
+    """
+    assert predictions.dtype == np.dtype('int')
+    # Find number of closed objects using skimage "label"
+    labeled_obj, num_obj = label(predictions)
+    # If more than one object is found, keep the largest one
+    if num_obj > 1:
+        # Keep the largest object
+            predictions[np.where(labeled_obj != (np.bincount(labeled_obj.flat)[1:].argmax() + 1))] = 0
+    return predictions
