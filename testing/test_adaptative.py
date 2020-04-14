ff104788db0ea5155e4360e6c37407485339d87e
andreanne-lemay <andreanne.lemay@polymtl.ca>
2020-04-13 22:24:51 -0400

    adapt test adaptative

--- testing/test_adaptative.py
+++ testing/test_adaptative.py
@@ -1,15 +1,14 @@
 import os
+
 import torch
+from medicaltorch import transforms as mt_transforms
 from torch.utils.data import DataLoader
 from torchvision import transforms
 
-from medicaltorch.filters import SliceFilter
-from medicaltorch import transforms as mt_transforms
-from medicaltorch import datasets as mt_datasets
-from ivadomed import adaptative as adaptative
-from ivadomed import utils
 import ivadomed.transforms as ivadomed_transforms
-
+from ivadomed import adaptative as imed_adaptative
+from ivadomed import utils as imed_utils
+from ivadomed import loader_utils as imed_loader_utils
 
 GPU_NUMBER = 0
 BATCH_SIZE = 4
@@ -26,16 +25,17 @@ def test_hdf5():
     print('[INFO]: Starting test ... \n')
     train_lst = ['sub-test001']
 
-    hdf5_file = adaptative.Bids_to_hdf5(PATH_BIDS,
-                                        subject_lst=train_lst,
-                                        hdf5_name='testing_data/mytestfile.hdf5',
-                                        target_suffix=["_lesion-manual"],
-                                        roi_suffix="_seg-manual",
-                                        contrast_lst=['T1w', 'T2w', 'T2star'],
-                                        metadata_choice="contrast",
-                                        contrast_balance={},
-                                        slice_axis=2,
-                                        slice_filter_fn=SliceFilter(filter_empty_input=True, filter_empty_mask=True))
+    hdf5_file = imed_adaptative.Bids_to_hdf5(PATH_BIDS,
+                                             subject_lst=train_lst,
+                                             hdf5_name='testing_data/mytestfile.hdf5',
+                                             target_suffix=["_lesion-manual"],
+                                             roi_suffix="_seg-manual",
+                                             contrast_lst=['T1w', 'T2w', 'T2star'],
+                                             metadata_choice="contrast",
+                                             contrast_balance={},
+                                             slice_axis=2,
+                                             slice_filter_fn=imed_utils.SliceFilter(filter_empty_input=True,
+                                                                                    filter_empty_mask=True))
 
     # Checking architecture
     def print_attrs(name, obj):
@@ -50,13 +50,13 @@ def test_hdf5():
     print('\n[INFO]: HDF5 file successfully generated.')
     print('[INFO]: Generating dataframe ...\n')
 
-    df = adaptative.Dataframe(hdf5=hdf5_file.hdf5_file,
-                              contrasts=['T1w', 'T2w', 'T2star'],
-                              path='testing_data/hdf5.csv',
-                              target_suffix=['T1w', 'T2w', 'T2star'],
-                              roi_suffix=['T1w', 'T2w', 'T2star'],
-                              dim=2,
-                              slices=True)
+    df = imed_adaptative.Dataframe(hdf5=hdf5_file.hdf5_file,
+                                   contrasts=['T1w', 'T2w', 'T2star'],
+                                   path='testing_data/hdf5.csv',
+                                   target_suffix=['T1w', 'T2w', 'T2star'],
+                                   roi_suffix=['T1w', 'T2w', 'T2star'],
+                                   dim=2,
+                                   slices=True)
     print(df.df)
 
     print('\n[INFO]: Dataframe successfully generated. ')
@@ -69,23 +69,24 @@ def test_hdf5():
     ]
     train_transform = transforms.Compose(training_transform_list)
 
-    dataset = adaptative.HDF5Dataset(root_dir=PATH_BIDS,
-                                     subject_lst=train_lst,
-                                     hdf5_name='testing_data/mytestfile.hdf5',
-                                     csv_name='testing_data/hdf5.csv',
-                                     target_suffix="_lesion-manual",
-                                     contrast_lst=['T1w', 'T2w', 'T2star'],
-                                     ram=False,
-                                     contrast_balance={},
-                                     slice_axis=2,
-                                     transform=train_transform,
-                                     metadata_choice=False,
-                                     dim=2,
-                                     slice_filter_fn=SliceFilter(filter_empty_input=True, filter_empty_mask=True),
-                                     canonical=True,
-                                     roi_suffix="_seg-manual",
-                                     target_lst=['T2w'],
-                                     roi_lst=['T2w'])
+    dataset = imed_adaptative.HDF5Dataset(root_dir=PATH_BIDS,
+                                          subject_lst=train_lst,
+                                          hdf5_name='testing_data/mytestfile.hdf5',
+                                          csv_name='testing_data/hdf5.csv',
+                                          target_suffix="_lesion-manual",
+                                          contrast_lst=['T1w', 'T2w', 'T2star'],
+                                          ram=False,
+                                          contrast_balance={},
+                                          slice_axis=2,
+                                          transform=train_transform,
+                                          metadata_choice=False,
+                                          dim=2,
+                                          slice_filter_fn=imed_utils.SliceFilter(filter_empty_input=True,
+                                                                                 filter_empty_mask=True),
+                                          canonical=True,
+                                          roi_suffix="_seg-manual",
+                                          target_lst=['T2w'],
+                                          roi_lst=['T2w'])
 
     dataset.load_into_ram(['T1w', 'T2w', 'T2star'])
     print("Dataset RAM status:")
@@ -104,7 +105,7 @@ def test_hdf5():
 
     train_loader = DataLoader(dataset, batch_size=BATCH_SIZE,
                               shuffle=False, pin_memory=True,
-                              collate_fn=mt_datasets.mt_collate,
+                              collate_fn=imed_loader_utils.mt_collate,
                               num_workers=1)
 
     for i, batch in enumerate(train_loader):
@@ -113,8 +114,8 @@ def test_hdf5():
         print("Batch = {}, {}".format(input_samples[0].shape, gt_samples[0].shape))
 
         if cuda_available:
-            var_input = utils.cuda(input_samples)
-            var_gt = utils.cuda(gt_samples, non_blocking=True)
+            var_input = imed_utils.cuda(input_samples)
+            var_gt = imed_utils.cuda(gt_samples, non_blocking=True)
         else:
             var_input = input_samples
             var_gt = gt_samples
