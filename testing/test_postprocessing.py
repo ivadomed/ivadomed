4d554de943d2874e4744fff2f5eaaa5f982680ce
Julien Cohen-Adad <jcohen@polymtl.ca>
2020-04-15 14:53:02 -0400

    test_postprocessing.py: Added test_fill_holes

--- testing/test_postprocessing.py
+++ testing/test_postprocessing.py
@@ -11,10 +11,10 @@ import ivadomed.postprocessing as postproc
 
 
 @pytest.fixture(scope="session")
-def nii_seg(size_arr=(9, 9, 9), pixdim=(1, 1, 1), dtype=np.float64, orientation='LPI', shape='rectangle',
-            radius_RL=2.0, radius_AP=1.0, zeroslice=[]):
+def nii_seg(size_arr=(15, 15, 9), pixdim=(1, 1, 1), dtype=np.float64, orientation='LPI', shape='rectangle',
+            radius_RL=3.0, radius_AP=2.0, zeroslice=[]):
     """Create a dummy nibabel object with a ellipse or rectangle of ones running from top to bottom in the 3rd
-    dimension. Add a voxel with value '1' outside the main object to test the keep largest object algorithms.
+    dimension.
     :param size_arr: tuple: (nx, ny, nz)
     :param pixdim: tuple: (px, py, pz)
     :param dtype: Numpy dtype.
@@ -40,9 +40,6 @@ def nii_seg(size_arr=(9, 9, 9), pixdim=(1, 1, 1), dtype=np.float64, orientation=
     # Zero specified slices
     if zeroslice is not []:
         data[:, :, zeroslice] = 0
-
-    # Add voxel outside of the main object
-    data[1, 1, 1] = 1
 
     # Apply Gaussian filter (to get soft seg)
     # TODO
@@ -70,12 +67,42 @@ def test_threshold(nii_seg):
 
 
 def test_keep_largest_object(nii_seg):
+    # Set a voxel to 1 at the corner to make sure it is set to 0 by the function
+    coord = (1, 1, 1)
+    nii_seg.dataobj[coord] = 1
+    # Test function with array input
     arr_seg_proc = postproc.keep_largest_object(np.asanyarray(nii_seg.dataobj))
     assert isinstance(arr_seg_proc, np.ndarray)
-    assert arr_seg_proc[1, 1, 1] == 0
+    assert arr_seg_proc[coord] == 0
+    # Make sure it works with nibabel input
+    nii_seg_proc = postproc.keep_largest_object(nii_seg)
+    assert isinstance(nii_seg_proc, nib.nifti1.Nifti1Image)
+    assert nii_seg_proc.dataobj[coord] == 0
 
 
 def test_keep_largest_object_per_slice(nii_seg):
+    # Set a voxel to 1 at the corner to make sure it is set to 0 by the function
+    coord = (1, 1, 1)
+    nii_seg.dataobj[coord] = 1
+    # Test function with array input
     arr_seg_proc = postproc.keep_largest_object_per_slice(np.asanyarray(nii_seg.dataobj), axis=2)
     assert isinstance(arr_seg_proc, np.ndarray)
-    assert arr_seg_proc[1, 1, 1] == 0
+    assert arr_seg_proc[coord] == 0
+    # Make sure it works with nibabel input
+    nii_seg_proc = postproc.keep_largest_object_per_slice(nii_seg)
+    assert isinstance(nii_seg_proc, nib.nifti1.Nifti1Image)
+    assert nii_seg_proc.dataobj[coord] == 0
+
+
+def test_fill_holes(nii_seg):
+    # Set a voxel to 0 in the middle of the segmentation to make sure it is set to 1 by the function
+    coord = (7, 7, 4)
+    nii_seg.dataobj[coord] = 0
+    # Test function with array input
+    arr_seg_proc = postproc.fill_holes(np.asanyarray(nii_seg.dataobj))
+    assert isinstance(arr_seg_proc, np.ndarray)
+    assert arr_seg_proc[coord] == 1
+    # make sure it works with nibabel input
+    nii_seg_proc = postproc.fill_holes(nii_seg)
+    assert isinstance(nii_seg_proc, nib.nifti1.Nifti1Image)
+    assert nii_seg_proc.dataobj[coord] == 1
