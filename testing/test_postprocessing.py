c0725ed25bf6a835170a5a680c36748ce17cce9b
Julien Cohen-Adad <jcohen@polymtl.ca>
2020-04-15 18:21:00 -0400

    test_postprocessing.py: Added blurring to test thresholding

--- testing/test_postprocessing.py
+++ testing/test_postprocessing.py
@@ -4,15 +4,15 @@
 
 
 import pytest
+import scipy
 import numpy as np
 import nibabel as nib
 
 import ivadomed.postprocessing as postproc
 
 
-@pytest.fixture(scope="function")
-def nii_seg(size_arr=(15, 15, 9), pixdim=(1, 1, 1), dtype=np.float64, orientation='LPI', shape='rectangle',
-            radius_RL=3.0, radius_AP=2.0, zeroslice=[]):
+def nii_dummy_seg(size_arr=(15, 15, 9), pixdim=(1, 1, 1), dtype=np.float64, orientation='LPI', shape='rectangle',
+            radius_RL=3.0, radius_AP=2.0, zeroslice=[], softseg=False):
     """Create a dummy nibabel object with a ellipse or rectangle of ones running from top to bottom in the 3rd
     dimension.
     :param size_arr: tuple: (nx, ny, nz)
@@ -23,7 +23,7 @@ def nii_seg(size_arr=(15, 15, 9), pixdim=(1, 1, 1), dtype=np.float64, orientatio
     :param radius_RL: float: 1st radius. With a, b = 50.0, 30.0 (in mm), theoretical CSA of ellipse is 4712.4
     :param radius_AP: float: 2nd radius
     :param zeroslice: list int: zero all slices listed in this param
-    :param debug: Write temp files for debug
+    :param softseg: Bool: Generate soft segmentation by applying blurring filter.
     :return: nibabel: Image object
     """
     # Create a 3d array, with dimensions corresponding to x: RL, y: AP, z: IS
@@ -36,36 +36,35 @@ def nii_seg(size_arr=(15, 15, 9), pixdim=(1, 1, 1), dtype=np.float64, orientatio
             data[:, :, iz] = ((abs(xx - nx / 2) <= radius_RL) & (abs(yy - ny / 2) <= radius_AP)) * 1
         if shape == 'ellipse':
             data[:, :, iz] = (((xx - nx / 2) / radius_RL) ** 2 + ((yy - ny / 2) / radius_AP) ** 2 <= 1) * 1
-
     # Zero specified slices
     if zeroslice is not []:
         data[:, :, zeroslice] = 0
-
     # Apply Gaussian filter (to get soft seg)
-    # TODO
-
+    if softseg:
+        kernel = np.ones((3, 3, 3)) / 27
+        data = scipy.ndimage.convolve(data, kernel)
     # Create nibabel object
     affine = np.eye(4)
-    # for i in range(3):
-    #     affine[i][i] = 1  # in [mm]
     nii = nib.nifti1.Nifti1Image(data, affine)
-
-
     # Change orientation
     # TODO
-
     return nii
 
 
+@pytest.mark.parametrize('nii_seg', [nii_dummy_seg(softseg=True)])
 def test_threshold(nii_seg):
     # input array
     arr_seg_proc = postproc.threshold_predictions(np.copy(np.asanyarray(nii_seg.dataobj)))
     assert isinstance(arr_seg_proc, np.ndarray)
+    # Before thresholding: [0.33333333, 0.66666667, 1.        ] --> after thresholding: [0, 1, 1]
+    assert np.array_equal(arr_seg_proc[4:7, 8, 4], np.array([0, 1, 1]))
     # input nibabel
     nii_seg_proc = postproc.threshold_predictions(nii_seg)
     assert isinstance(nii_seg_proc, nib.nifti1.Nifti1Image)
+    assert np.array_equal(nii_seg_proc.get_fdata()[4:7, 8, 4], np.array([0, 1, 1]))
 
 
+@pytest.mark.parametrize('nii_seg', [nii_dummy_seg()])
 def test_keep_largest_object(nii_seg):
     # Set a voxel to 1 at the corner to make sure it is set to 0 by the function
     coord = (1, 1, 1)
@@ -80,6 +79,7 @@ def test_keep_largest_object(nii_seg):
     assert nii_seg_proc.dataobj[coord] == 0
 
 
+@pytest.mark.parametrize('nii_seg', [nii_dummy_seg()])
 def test_keep_largest_object_per_slice(nii_seg):
     # Set a voxel to 1 at the corner to make sure it is set to 0 by the function
     coord = (1, 1, 1)
@@ -94,6 +94,7 @@ def test_keep_largest_object_per_slice(nii_seg):
     assert nii_seg_proc.dataobj[coord] == 0
 
 
+@pytest.mark.parametrize('nii_seg', [nii_dummy_seg()])
 def test_fill_holes(nii_seg):
     # Set a voxel to 0 in the middle of the segmentation to make sure it is set to 1 by the function
     coord = (7, 7, 4)
@@ -108,6 +109,7 @@ def test_fill_holes(nii_seg):
     assert nii_seg_proc.dataobj[coord] == 1
 
 
+@pytest.mark.parametrize('nii_seg', [nii_dummy_seg()])
 def test_mask_predictions(nii_seg):
     # create nii object with a voxel of 0 somewhere in the middle
     nii_seg_mask = nib.nifti1.Nifti1Image(np.copy(np.asanyarray(nii_seg.dataobj)), nii_seg.affine)
