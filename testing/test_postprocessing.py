68697de7949423db4178c44881fdad51df1e9854
charleygros <charley.gros@gmail.com>
2020-04-17 22:26:23 -0400

    testing/test_postprocessing.py: test_keep_largest_per_slice soft

--- testing/test_postprocessing.py
+++ testing/test_postprocessing.py
@@ -82,8 +82,9 @@ def test_keep_largest_object(nii_seg):
     assert nii_seg_proc.dataobj[coord] == 0
 
 
-@pytest.mark.parametrize('nii_seg', [nii_dummy_seg()])
+@pytest.mark.parametrize('nii_seg', [nii_dummy_seg(), nii_dummy_seg(softseg=True)])
 def test_keep_largest_object_per_slice(nii_seg):
+    nii_seg_copy = np.copy(nii_seg)
     # Set a voxel to 1 at the corner to make sure it is set to 0 by the function
     coord = (1, 1, 1)
     nii_seg.dataobj[coord] = 1
@@ -91,6 +92,8 @@ def test_keep_largest_object_per_slice(nii_seg):
     arr_seg_proc = postproc.keep_largest_object_per_slice(np.copy(np.asanyarray(nii_seg.dataobj)), axis=2)
     assert isinstance(arr_seg_proc, np.ndarray)
     assert arr_seg_proc[coord] == 0
+    # Make sure it equals the input data, in particular: still binary / soft if the input was binary / soft
+    assert np.array_equal(nii_seg.dataobj, arr_seg_proc)
     # Make sure it works with nibabel input
     nii_seg_proc = postproc.keep_largest_object_per_slice(nii_seg)
     assert isinstance(nii_seg_proc, nib.nifti1.Nifti1Image)
