619817a60dac9f377e8e28f50be016d3847f2a3c
andreanne-lemay <andreanne.lemay@polymtl.ca>
2020-05-10 01:38:20 -0400

    utils: if rdict_undo[gt] is already numpy array

--- ivadomed/utils.py
+++ ivadomed/utils.py
@@ -703,7 +703,10 @@ def segment_volume(folder_model, fname_image, fname_roi=None):
 
             # Add new segmented slice to preds_list
             # Convert PIL to numpy
-            pred_cur = np.array(pil_list_to_numpy(rdict_undo['gt']))
+            if isinstance(rdict_undo['gt'], list):
+                pred_cur = np.array(pil_list_to_numpy(rdict_undo['gt']))
+            else:
+                pred_cur = rdict_undo['gt']
             preds_list.append(pred_cur)
             # Store the slice index of pred_cur in the original 3D image
             sliceIdx_list.append(int(rdict_undo['input_metadata']['slice_index']))
