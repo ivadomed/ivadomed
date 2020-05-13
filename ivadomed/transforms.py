99e13f7226fee228201be8c83900d03ffa2c3454
charleygros <charley.gros@gmail.com>
2020-05-13 13:49:21 +1000

    ivadomed/transforms.py: adjust crop

--- ivadomed/transforms.py
+++ ivadomed/transforms.py
@@ -230,11 +230,95 @@ class NormalizeInstance(ImedTransform):
         return data_out, metadata
 
 
+class CroppableArray(np.ndarray):
+    """From: https://stackoverflow.com/a/41155020/13306686"""
+    def __getitem__(self, item):
+
+        all_in_slices = []
+        pad = []
+        for dim in range(self.ndim):
+            # If the slice has no length then it's a single argument.
+            # If it's just an integer then we just return, this is
+            # needed for the representation to work properly
+            # If it's not then create a list containing None-slices
+            # for dim>=1 and continue down the loop
+            try:
+                len(item)
+            except TypeError:
+                if isinstance(item, int):
+                    return super().__getitem__(item)
+                newitem = [slice(None)]*self.ndim
+                newitem[0] = item
+                item = newitem
+            # We're out of items, just append noop slices
+            if dim >= len(item):
+                all_in_slices.append(slice(0, self.shape[dim]))
+                pad.append((0, 0))
+            # We're dealing with an integer (no padding even if it's
+            # out of bounds)
+            if isinstance(item[dim], int):
+                all_in_slices.append(slice(item[dim], item[dim]+1))
+                pad.append((0, 0))
+            # Dealing with a slice, here it get's complicated, we need
+            # to correctly deal with None start/stop as well as with
+            # out-of-bound values and correct padding
+            elif isinstance(item[dim], slice):
+                # Placeholders for values
+                start, stop = 0, self.shape[dim]
+                this_pad = [0, 0]
+                if item[dim].start is None:
+                    start = 0
+                else:
+                    if item[dim].start < 0:
+                        this_pad[0] = -item[dim].start
+                        start = 0
+                    else:
+                        start = item[dim].start
+                if item[dim].stop is None:
+                    stop = self.shape[dim]
+                else:
+                    if item[dim].stop > self.shape[dim]:
+                        this_pad[1] = item[dim].stop - self.shape[dim]
+                        stop = self.shape[dim]
+                    else:
+                        stop = item[dim].stop
+                all_in_slices.append(slice(start, stop))
+                pad.append(tuple(this_pad))
+
+        # Let numpy deal with slicing
+        ret = super().__getitem__(tuple(all_in_slices))
+        # and padding
+        ret = np.pad(ret, tuple(pad), mode='constant', constant_values=0)
+
+        return ret
+
+
 class Crop2D(ImedTransform):
     def __init__(self, size):
         self.size = size
 
-    # TODO: negative coords
+    @staticmethod
+    def _adjust_padding(npad, sample):
+        npad_out_tuple = []
+        for idx_dim, tuple_pad in enumerate(npad):
+            pad_start, pad_end = tuple_pad
+            if pad_start < 0 or pad_end < 0:
+                # Move axis of interest
+                sample_reorient = np.swapaxes(sample, 0, idx_dim)
+                # Adjust pad and crop
+                if pad_start < 0:
+                    sample_crop = sample_reorient[abs(pad_start):, :]
+                    pad_start = 0
+                if pad_end < 0:
+                    sample_crop = sample_reorient[:pad_end, :]
+                    pad_end = 0
+                # Reorient
+                sample = np.swapaxes(sample_crop, 0, idx_dim)
+
+            npad_out_tuple.append((pad_start, pad_end))
+
+        return npad_out_tuple, sample
+
     @list_capable
     def __call__(self, sample, metadata={}):
         # Get params
@@ -242,41 +326,31 @@ class Crop2D(ImedTransform):
         fh, fw, h, w = metadata['crop_params']
 
         # Crop data
-        data_out = sample[fh:fh+th, fw:fw+tw]
-
-        # Compute params to undo transform
-        pad_left = fw
-        pad_right = w - pad_left - tw
-        pad_top = fh
-        pad_bottom = h - pad_top - th
-
-        # Pad image if crop out of boundaries
-        ch, cw = data_out.shape
-        if (ch, cw) != (th, tw):
-            h_offset = (th - ch) / 2.
-            h_eps = 1 if h_offset % 1 != 0 else 0
-            w_offset = (tw - cw) / 2.
-            w_eps = 1 if w_offset % 1 != 0 else 0
-            npad = ((int(h_offset) + h_eps, int(h_offset)),
-                    (int(w_offset) + w_eps, int(w_offset)))
-            data_out = np.pad(data_out,
-                              pad_width=npad,
-                              mode='constant',
-                              constant_values=np.min(data_out)).astype(sample.dtype)
-
-        # Save undo params
-        metadata['crop_undo'] = [(pad_top, pad_bottom), (pad_left, pad_right)]
+        # Note we use here CroppableArray in order to deal with "out of boundaries" crop
+        # e.g. if fh is negative or fh+th out of bounds, then it will pad
+        data_out = sample.view(CroppableArray)[fh:fh+th, fw:fw+tw]
 
         return data_out, metadata
 
     @list_capable
     def undo_transform(self, sample, metadata):
         # Get crop params
-        npad = metadata["crop_undo"]
+        th, tw = self.size
+        fh, fw, h, w = metadata["crop_params"]
+
+        # Compute params to undo transform
+        pad_left = fw
+        pad_right = w - pad_left - tw
+        pad_top = fh
+        pad_bottom = h - pad_top - th
+        npad = [(pad_top, pad_bottom), (pad_left, pad_right)]
+
+        # Check and adjust npad if needed, i.e. if crop out of boundaries
+        npad_adj, sample_adj = self._adjust_padding(npad, sample.copy())
 
         # Apply padding
-        data_out = np.pad(sample,
-                          npad,
+        data_out = np.pad(sample_adj,
+                          npad_adj,
                           mode='constant',
                           constant_values=0).astype(sample.dtype)
 
