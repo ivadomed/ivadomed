8b6bad58edc8f5daa67519aa985866b83d83f368
lrouhier <lucas@DESKTOP-NP669HT.localdomain>
2020-05-14 18:50:46 -0400

    we dont label manually one and two

--- labeling/label_disc/label_script.sh
+++ labeling/label_disc/label_script.sh
@@ -15,10 +15,10 @@ do
 
 		if test -f "$path_ilabel"; then
     		echo "$path_ilabel exist"
-    		sct_label_utils -i "$path_input" -ilabel "$path_ilabel" -create-viewer 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20 -o "$6"/"$line"/anat/"$line"/"$line""$4"
+    		sct_label_utils -i "$path_input" -ilabel "$path_ilabel" -create-viewer 3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20 -o "$6"/"$line"/anat/"$line"/"$line""$4"
 	
 		else
-			sct_label_utils -i "$path_input" -create-viewer 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20 -o "$6"/"$line"/anat/"$line""$4"
+			sct_label_utils -i "$path_input" -create-viewer 3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20 -o "$6"/"$line"/anat/"$line""$4"
 		fi
 
 		echo "$line" >> list_done.txt
