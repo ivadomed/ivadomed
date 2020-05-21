7386c9a628bb70789a502cc3b83031c416fd6b5b
andreanne-lemay <andreanne.lemay@polymtl.ca>
2020-05-21 00:16:08 -0400

    test_segment_volume: include 1 and 0 in value range

--- testing/test_segment_volume.py
+++ testing/test_segment_volume.py
@@ -99,7 +99,7 @@ def test_segment_volume_3d():
 
     nib_img = imed_utils.segment_volume(PATH_MODEL, IMAGE_PATH)
     assert np.squeeze(nib_img.get_fdata()).shape == nib.load(IMAGE_PATH).shape
-    assert (nib_img.dataobj.max() < 1.0) and (nib_img.dataobj.max() > 0.0)
+    assert (nib_img.dataobj.max() <= 1.0) and (nib_img.dataobj.max() >= 0.0)
     assert nib_img.dataobj.dtype == 'float32'
 
     shutil.rmtree(PATH_MODEL)
