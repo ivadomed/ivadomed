8e79649029f6f966ebcf0030e826cc13a6557726
charleygros <charley.gros@gmail.com>
2020-06-03 19:27:07 +1000

    test_training_time: clean loop

--- testing/test_training_time.py
+++ testing/test_training_time.py
@@ -102,87 +102,78 @@ def test_unet_time(train_lst, target_lst, config):
     model_class = getattr(imed_models, model_params["name"])
     model = model_class(**model_params)
 
+    print("Training {}".format(model_params["name"]))
+    if cuda_available:
+        model.cuda()
 
-    for model, train_loader, film_bool, model_name in model_list:
-        print("Training {}".format(model_name))
-        if cuda_available:
-            model.cuda()
+    step_scheduler_batch = False
+    # TODO: Add optim in pytest
+    optimizer = optim.Adam(model.parameters(), lr=INIT_LR)
+    scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, N_EPOCHS)
 
-        step_scheduler_batch = False
-        optimizer = optim.Adam(model.parameters(), lr=INIT_LR)
-        scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, N_EPOCHS)
+    load_lst, pred_lst, opt_lst, schedul_lst, init_lst, gen_lst = [], [], [], [], [], []
+    for epoch in tqdm(range(1, N_EPOCHS + 1), desc="Training"):
+        start_time = time.time()
 
-        load_lst, pred_lst, opt_lst, schedul_lst, init_lst, gen_lst = [], [], [], [], [], []
-        for epoch in tqdm(range(1, N_EPOCHS + 1), desc="Training"):
-            start_time = time.time()
+        start_init = time.time()
 
-            start_init = time.time()
-            lr = scheduler.get_last_lr()[0]
-            model.train()
-            tot_init = time.time() - start_init
-            init_lst.append(tot_init)
+        model.train()
 
-            num_steps = 0
-            start_gen = 0
-            for i, batch in enumerate(train_loader):
-                if i > 0:
-                    tot_gen = time.time() - start_gen
-                    gen_lst.append(tot_gen)
+        tot_init = time.time() - start_init
+        init_lst.append(tot_init)
 
-                start_load = time.time()
-                input_samples, gt_samples = batch["input"], batch["gt"]
-                if cuda_available:
-                    var_input = input_samples.cuda()
-                    var_gt = gt_samples.cuda(non_blocking=True)
-                else:
-                    var_input = input_samples
-                    var_gt = gt_samples
+        num_steps = 0
+        start_gen = 0
+        for i, batch in enumerate(train_loader):
+            if i > 0:
+                tot_gen = time.time() - start_gen
+                gen_lst.append(tot_gen)
 
-                sample_metadata = batch["input_metadata"]
-                if film_bool:
-                    var_metadata = [train_onehotencoder.transform([sample_metadata[0][k]['film_input']]).tolist()[0]
-                                    for k in range(len(sample_metadata[0]))]
-                tot_load = time.time() - start_load
-                load_lst.append(tot_load)
+            start_load = time.time()
+            input_samples, gt_samples = batch["input"], batch["gt"]
+            if cuda_available:
+                var_input = input_samples.cuda()
+                var_gt = gt_samples.cuda(non_blocking=True)
+            else:
+                var_input = input_samples
+                var_gt = gt_samples
 
-                start_pred = time.time()
-                if film_bool:
-                    # Input the metadata related to the input samples
-                    preds = model(var_input, var_metadata)
-                else:
-                    preds = model(var_input)
-                tot_pred = time.time() - start_pred
-                pred_lst.append(tot_pred)
+            tot_load = time.time() - start_load
+            load_lst.append(tot_load)
 
-                start_opt = time.time()
-                loss = - losses.dice_loss(preds, var_gt)
+            start_pred = time.time()
+            preds = model(var_input)
+            tot_pred = time.time() - start_pred
+            pred_lst.append(tot_pred)
 
-                optimizer.zero_grad()
-                loss.backward()
+            start_opt = time.time()
+            loss = losses.DiceLoss(preds, var_gt)
 
-                optimizer.step()
-                if step_scheduler_batch:
-                    scheduler.step()
-
-                num_steps += 1
-                tot_opt = time.time() - start_opt
-                opt_lst.append(tot_opt)
-
-                start_gen = time.time()
-
-            start_schedul = time.time()
-            if not step_scheduler_batch:
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            if step_scheduler_batch:
                 scheduler.step()
-            tot_schedul = time.time() - start_schedul
-            schedul_lst.append(tot_schedul)
 
-            end_time = time.time()
-            total_time = end_time - start_time
-            tqdm.write("Epoch {} took {:.2f} seconds.".format(epoch, total_time))
+            num_steps += 1
+            tot_opt = time.time() - start_opt
+            opt_lst.append(tot_opt)
 
-        print('Mean SD init {} -- {}'.format(np.mean(init_lst), np.std(init_lst)))
-        print('Mean SD load {} -- {}'.format(np.mean(load_lst), np.std(load_lst)))
-        print('Mean SD pred {} -- {}'.format(np.mean(pred_lst), np.std(pred_lst)))
-        print('Mean SDopt {} --  {}'.format(np.mean(opt_lst), np.std(opt_lst)))
-        print('Mean SD gen {} -- {}'.format(np.mean(gen_lst), np.std(gen_lst)))
-        print('Mean SD scheduler {} -- {}'.format(np.mean(schedul_lst), np.std(schedul_lst)))
+            start_gen = time.time()
+
+        start_schedul = time.time()
+        if not step_scheduler_batch:
+            scheduler.step()
+        tot_schedul = time.time() - start_schedul
+        schedul_lst.append(tot_schedul)
+
+        end_time = time.time()
+        total_time = end_time - start_time
+        tqdm.write("Epoch {} took {:.2f} seconds.".format(epoch, total_time))
+
+    print('Mean SD init {} -- {}'.format(np.mean(init_lst), np.std(init_lst)))
+    print('Mean SD load {} -- {}'.format(np.mean(load_lst), np.std(load_lst)))
+    print('Mean SD pred {} -- {}'.format(np.mean(pred_lst), np.std(pred_lst)))
+    print('Mean SDopt {} --  {}'.format(np.mean(opt_lst), np.std(opt_lst)))
+    print('Mean SD gen {} -- {}'.format(np.mean(gen_lst), np.std(gen_lst)))
+    print('Mean SD scheduler {} -- {}'.format(np.mean(schedul_lst), np.std(schedul_lst)))
