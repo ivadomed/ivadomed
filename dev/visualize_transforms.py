788ed4f27254ee63b58e1b8acdc4e0e28bd18418
charleygros <charley.gros@gmail.com>
2020-06-09 21:41:53 +1000

    visualize_transforms.py: reorient slice_axis

--- dev/visualize_transforms.py
+++ dev/visualize_transforms.py
@@ -4,7 +4,7 @@
 # This script apply a series of transforms to 2D slices extracted from an input image,
 #   and save as png the resulting sample after each transform.
 #
-# Usage: python dev/visualize_transforms.py -i <input_filename>
+# Usage: python dev/visualize_transforms.py -i <input_filename> -a <int>
 #
 ##############################################################
 
@@ -12,11 +12,15 @@ import argparse
 import nibabel as nib
 import numpy as np
 
+from ivadomed.loader import utils as imed_loader_utils
+
 
 def get_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", "--input", required=True,
                         help="Input image filename.")
+    parser.add_argument("-a", "--axis", required=True, type=int,
+                        help="Slice axis for slice extraction: 0 for sagittal, 1 for coronal, 2 for axial.")
     return parser
 
 
@@ -29,13 +33,17 @@ def run_visualization(args):
     Returns:
         None
     """
+    # Get params
     fname_input = args.i
+    axis = args.a
     # Load image
     input_img = nib.load(fname_input)
     # Reorient as canonical
     input_img = nib.as_closest_canonical(input_img)
     # Get input data
     input_data = input_img.get_fdata(dtype=np.float32)
+    # Reorient data
+    input_data = imed_loader_utils.orient_img_hwd(input_data, slice_axis=axis)
 
 
 if __name__ == '__main__':
