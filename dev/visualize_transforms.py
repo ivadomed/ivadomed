8dd32170ab0687702ce9916012e9ad8269f4f629
charleygros <charley.gros@gmail.com>
2020-06-09 20:29:45 +1000

    dev/visualize_transforms.py: init file docstring

---
+++ dev/visualize_transforms.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+##############################################################
+#
+# This script apply a series of transforms to 2D slices extracted from an input image,
+#   and save as png the resulting sample after each transform.
+#
+# Usage: python dev/visualize_transforms.py -i <input_filename>
+#
+##############################################################
