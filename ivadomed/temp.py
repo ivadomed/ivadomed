8a64f25d23eb2801a2e9d54a4aa1aa2a30b3612d
andreanne-lemay <andreanne.lemay@polymtl.ca>
2020-06-22 18:57:18 -0400

    remove unwanted file

--- ivadomed/temp.py
+++
@@ -1,6 +0,0 @@
-import os
-path_to_img = "/home/andreanne/Documents/dataset/tumor_segmentation_masks/results/data/sub-Astr144/anat/sub-Astr144_T2w.nii.gz"
-model1 = "/home/andreanne/Documents/models/sc_model_30mm_2"
-model2 = "/home/andreanne/Documents/models/t2_tumor"
-
-os.system(f"sct_deepseg -i {path_to_img} -model {model1}")
