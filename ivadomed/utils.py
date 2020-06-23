159aedb96bedc23b469fca106b18763e9992c1c8
andreanne-lemay <andreanne.lemay@polymtl.ca>
2020-06-23 16:26:22 -0400

    add metadata in segment_volume

--- ivadomed/utils.py
+++ ivadomed/utils.py
@@ -440,9 +440,8 @@ def segment_volume(folder_model, fname_image, fname_prior=None, gpu_number=0):
     if fname_roi is None and 'filter_empty_mask' in loader_params["slice_filter_params"]:
         print("\nWARNING: fname_roi has not been specified, then the entire volume is processed.")
         loader_params["slice_filter_params"]["filter_empty_mask"] = False
-        #filename_pairs = [([fname_image], None, None, [{}])]
 
-    filename_pairs = [([fname_image], None, fname_roi, [{}])]
+    filename_pairs = [([fname_image], None, fname_roi, [metadata])]
 
     kernel_3D = bool('UNet3D' in context and context['UNet3D']['applied'])
     if kernel_3D:
