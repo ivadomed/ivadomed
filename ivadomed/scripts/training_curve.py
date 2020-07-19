35292f9946d175a1fbc946908527bef3cdbcfa66
charleygros <charley.gros@gmail.com>
2020-07-20 08:25:02 +1000

    training_curve: docstring typo

--- ivadomed/scripts/training_curve.py
+++ ivadomed/scripts/training_curve.py
@@ -15,17 +15,13 @@ def get_parser():
 def run_plot_training_curves(input_folder):
     """Utility function to XX.
 
-    Data augmentation is a key part of the Deep Learning training scheme. This script aims at facilitating the
-    fine-tuning of data augmentation parameters. To do so, this script provides a step-by-step visualization of the
-    transformations that are applied on data.
-
-    This function applies a series of transformations (defined in a configuration file
-    ``-c``) to ``-n`` 2D slices randomly extracted from an input image (``-i``), and save as png the resulting sample
-    after each transform.
+    XX
 
     For example::
 
         ivadomed_XX
+
+    XX
 
     .. image:: ../../images/XX
         :width: 600px
