b1ad6a8978487a4515eaec2af652a4bed9881dad
lrouhier <lucas.rouhier@telecom-paristech.fr>
2020-07-20 11:51:38 -0400

    test with error values

--- testing/test_metrics.py
+++ testing/test_metrics.py
@@ -19,3 +19,37 @@ def test_haussdorf_4d():
     image = np.array([[[1, 1], [1, 1]], [[0, 0], [0, 0]]])
     results = imed_metrics.hausdorff_score(image, image)
 
+
+def test_err_prec():
+    # create fake image
+    image = np.array([[[0, 0], [0, 0]], [[0, 0], [0, 0]]])
+    image_2 = np.array([[[0, 0], [0, 0]], [[0, 0], [0, 0]]])
+    results = imed_metrics.precision_score(image, image_2)
+    assert results == 0.0
+
+
+def test_err_rec():
+    # create fake image
+    image = np.array([[[0, 0], [0, 0]], [[0, 0], [0, 0]]])
+    image_2 = np.array([[[0, 0], [0, 0]], [[0, 0], [0, 0]]])
+    results = imed_metrics.recall_score(image, image_2, err_value=1)
+    assert results == 1
+
+
+def test_err_spec():
+    # create fake image
+    image = np.array([[[1, 1], [1, 1]], [[1, 1], [1, 1]]])
+    image_2 = np.array([[[1, 1], [1, 1]], [[1, 1], [1, 1]]])
+    results = imed_metrics.specificity_score(image, image_2, err_value=12)
+    assert results == 12
+
+
+def test_err_iou():
+    image = np.array([[[0, 0], [0, 0]], [[0, 0], [0, 0]]])
+    image_2 = np.array([[[0, 0], [0, 0]], [[0, 0], [0, 0]]])
+    results = imed_metrics.intersection_over_union(image, image_2, err_value=12)
+    assert results == 12
+
+
+
+
