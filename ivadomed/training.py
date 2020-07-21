dda312925c7fe320cb8a5ca37ddb03a557a5c589
charleygros <charley.gros@gmail.com>
2020-07-21 22:37:49 +1000

    training: add frame to gif

--- ivadomed/training.py
+++ ivadomed/training.py
@@ -209,7 +209,12 @@ def train(model_params, dataset_train, dataset_val, training_params, log_directo
                     val_dice_loss_total += loss_dice_fct(preds, gt_samples).item()
 
                     # Add frame to GIF
-                    # TODO
+                    for i_ in range(len(input_samples)):
+                        im, pr, met = input_samples[i_].cpu().numpy()[0], preds[i_].cpu().numpy()[0], batch["input_metadata"][i_][0]
+                        for i_gif in range(n_gif):
+                            if gif_dict["image_path"][i_gif] == met.__getitem__('input_filenames') and gif_dict["slice_id"][i_gif] == met.__ge$
+                                overlap = imed_utils.overlap_im_seg(im, pr)
+                                gif_dict["gif"][i_gif].add(overlap, label=str(epoch))
 
                 num_steps += 1
 
