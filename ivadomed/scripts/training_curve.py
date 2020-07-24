b1bf6a771c56728fb702d4cd726e65e001c28be6
charleygros <charley.gros@gmail.com>
2020-07-24 10:20:53 +1000

    training_curve: y_lim_loss

--- ivadomed/scripts/training_curve.py
+++ ivadomed/scripts/training_curve.py
@@ -21,6 +21,10 @@ def get_parser():
                         help="Multiple log directories are considered: all available folders with -i as "
                              "prefix. The plot represents the mean value (hard line) surrounded by the standard "
                              "deviation envelope.")
+    parser.add_argument("-y", "--ylim_loss", required=False, type=str,
+                        help="Indicates the limits on the y-axis for the loss plots, otherwise these limits are "
+                             "automatically defined. Please separate the lower and the upper limit by a comma, eg -1,0."
+                             "Note: for the validation metrics: the y-limits are always 0.0 and 1.0.")
     parser.add_argument("-o", "--output", required=True, type=str,
                         help="Output folder.")
     return parser
@@ -72,7 +76,7 @@ def get_data(event_dict):
     return metrics_df
 
 
-def plot_curve(data_list, y_label, fig_ax, subplot_title):
+def plot_curve(data_list, y_label, fig_ax, subplot_title, y_lim=None):
     """Plot curve of metrics or losses for each epoch.
 
     Args:
@@ -80,6 +84,7 @@ def plot_curve(data_list, y_label, fig_ax, subplot_title):
         y_label (str): Label for the y-axis.
         fig_ax (plt.subplot):
         subplot_title (str): Title of the subplot
+        y_lim (list): List of the lower and upper limits of the y-axis.
     """
     # Create count of the number of epochs
     max_nb_epoch = max([len(data_list[i]) for i in range(len(data_list))])
@@ -99,13 +104,13 @@ def plot_curve(data_list, y_label, fig_ax, subplot_title):
     fig_ax.grid(linestyle='dotted')
     fig_ax.set_xlabel('Epoch')
     fig_ax.set_ylabel(y_label)
-    if y_label != "loss":
-        fig_ax.set_ylim([0, 1])
+    if y_lim is not None:
+        fig_ax.set_ylim(y_lim)
     fig_ax.set_xlim([1, max_nb_epoch])
     fig_ax.title.set_text('\n'.join(wrap(subplot_title, 60)))
 
 
-def run_plot_training_curves(input_folder, output_folder, multiple_training=False):
+def run_plot_training_curves(input_folder, output_folder, multiple_training=False, y_lim_loss=None):
     """Utility function to plot the training curves.
 
     This function uses the TensorFlow summary that is generated during a training to plot for each epoch:
@@ -160,11 +165,11 @@ def run_plot_training_curves(input_folder, output_folder, multiple_training=Fals
         input_folder = os.path.expanduser(input_folder)
         # Find training folders:
         if multiple_training:
-            prefix = input_folder.split('/')[-1]
+            prefix = str(input_folder.split('/')[-1])
             input_folder = '/'.join(input_folder.split('/')[:-1])
             input_folder_list = [os.path.join(input_folder, f) for f in os.listdir(input_folder) if f.startswith(prefix)]
         else:
-            prefix = input_folder.split('/')[-1]
+            prefix = str(input_folder.split('/')[-1])
             input_folder_list = [input_folder]
 
         events_df_list = []
@@ -184,9 +189,10 @@ def run_plot_training_curves(input_folder, output_folder, multiple_training=Fals
             plt_dict[os.path.join(output_folder, "losses.png")] = plt.figure(figsize=(10 * n_cols, 5 * n_rows))
         ax = plt_dict[os.path.join(output_folder, "losses.png")].add_subplot(n_rows, n_cols, i_subplot + 1)
         plot_curve([df[loss_keys] for df in events_df_list],
-                   "loss",
-                   ax,
-                   prefix)
+                   y_label="loss",
+                   fig_ax=ax,
+                   subplot_title=prefix,
+                   y_lim=y_lim_loss)
 
         # Plot each validation metric separetly
         for tag in events_df_list[0].keys():
@@ -194,10 +200,11 @@ def run_plot_training_curves(input_folder, output_folder, multiple_training=Fals
                 if i_subplot == 0:  # Init plot
                     plt_dict[os.path.join(output_folder, tag+".png")] = plt.figure(figsize=(10 * n_cols, 5 * n_rows))
                 ax = plt_dict[os.path.join(output_folder, tag+".png")].add_subplot(n_rows, n_cols, i_subplot+1)
-                plot_curve([df[[tag]] for df in events_df_list],
-                           tag,
-                           ax,
-                           prefix)
+                plot_curve(data_list=[df[[tag]] for df in events_df_list],
+                           y_label=tag,
+                           fig_ax=ax,
+                           subplot_title=prefix,
+                           y_lim=[0, 1])
 
     for fname_out in plt_dict:
         plt_dict[fname_out].savefig(fname_out)
@@ -209,8 +216,9 @@ def main():
     input_folder = args.input
     multiple = args.multiple
     output_folder = args.output
+    y_lim_loss = [int(y) for y in args.y_lim_loss.split(',')] if args.y_lim_loss else None
     # Run script
-    run_plot_training_curves(input_folder, output_folder, multiple)
+    run_plot_training_curves(input_folder, output_folder, multiple, y_lim_loss)
 
 
 if __name__ == '__main__':
