66a1786538da59d6fadc6584799a7a7b09d00eb7
Lucas Rouhier <lucas.rouhier@telecom-paristech.fr>
2020-07-27 14:36:44 -0400

    creating dummy model

--- testing/ivado_test.py
+++ testing/ivado_test.py
@@ -1,4 +1,16 @@
 #Add custom tests here
+# this test is run first so we will also do a little setup here
+
+import ivadomed.models as imed_models
+import torch
+import os
 
 def test_sample():
     assert 1 == 1
+
+
+def test_model_creation():
+    model = imed_models.Unet()
+    torch.save(model, "testing_data/model_unet_test.pt")
+    assert os.path.isfile("testing_data/model_unet_test.pt")
+
