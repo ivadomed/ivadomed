28d58d9c9ec42f73fb37f498f4f2e19ceb07a672
andreanne-lemay <andreanne.lemay@polymtl.ca>
2020-07-28 13:07:29 -0400

    compute average and std intra sub

--- dev/reproducibility.py
+++ dev/reproducibility.py
@@ -2,6 +2,7 @@ import argparse
 import json
 import shutil
 import pandas as pd
+import numpy as np
 import os
 
 from ivadomed import main as ivado
@@ -10,14 +11,13 @@ from ivadomed import main as ivado
 def get_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument("-c", "--config", required=True, help="Base config file path.")
-    parser.add_argument("-b", "--bids-path", required=True, help="Bids path where the GT are located.")
     parser.add_argument("-n", "--iterations", default=10, type=int, help="Number of Monte Carlo iterations.")
     parser.add_argument("-o", "--output-path", default="output_reproducibility.csv", type=str,
                         help="Output directory to save final csv file.")
     return parser
 
 
-def reproducibility_pipeline(config):
+def get_results(config):
     with open(config, "r") as fhandle:
         context = json.load(fhandle)
 
@@ -35,9 +35,16 @@ def main():
     args = parser.parse_args()
 
     df_list = []
+    metrics = []
     for i in range(int(args.iterations)):
-        df = reproducibility_pipeline(args.config)
-        df_list.append(df)
+        df = get_results(args.config)
+        metrics = list(df.columns)
+        df_list.append(np.array(df))
+
+    # Get average and std for each subject (intra subject), then average on all subjects
+    average = np.average(np.average(np.array(df_list), axis=0), axis=1)
+    std = np.average(np.std(np.array(df_list), axis=0), axis=1)
+    pd.DataFrame([average, std], index=metrics, columns=["mean", "std"])
 
 
 if __name__ == '__main__':
