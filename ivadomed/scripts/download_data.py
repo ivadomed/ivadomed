3fafe8f1de458faa6c6637a944138e12d7927047
lrouhier <lucas.rouhier@telecom-paristech.fr>
2020-07-28 10:48:25 -0400

    wrong url. forgot comma

--- ivadomed/scripts/download_data.py
+++ ivadomed/scripts/download_data.py
@@ -204,13 +204,13 @@ def main(args=None):
     # If exists, favour release artifact straight from github
 
     dict_url = {
-        "data_example_spinegeneric": ["https://github.com/ivadomed/data_example_spinegeneric/releases/tag/r20200907"],
+        "data_example_spinegeneric": ["https://github.com/ivadomed/data_example_spinegeneric/archive/r20200907.zip"],
         "data-testing": ["https://github.com/ivadomed/data-testing/archive/master.zip"],
-        "t2_tumor": ["https://github.com/ivadomed/t2_tumor/releases/tag/r20200621"],
-        "t2star_sc": ["https://github.com/ivadomed/t2star_sc/releases/tag/r20200622"],
-        "mice_uqueensland_gm": ["https://github.com/ivadomed/mice_uqueensland_gm/releases/tag/r20200622 "],
-        "mice_uqueensland_sc": ["https://github.com/ivadomed/mice_uqueensland_sc/releases/tag/r20200622"],
-        "findcord_tumor": ["https://github.com/ivadomed/findcord_tumor/releases/tag/r20200621"]
+        "t2_tumor": ["https://github.com/ivadomed/t2_tumor/archive/r20200621.zip"],
+        "t2star_sc": ["https://github.com/ivadomed/t2star_sc/archive/r20200622.zip"],
+        "mice_uqueensland_gm": ["https://github.com/ivadomed/mice_uqueensland_gm/rarchive/r20200622.zip "],
+        "mice_uqueensland_sc": ["https://github.com/ivadomed/mice_uqueensland_sc/archive/r20200622.zip"],
+        "findcord_tumor": ["https://github.com/ivadomed/findcord_tumor/archive/r20200621.zip"]
     }
 
     if args is None:
