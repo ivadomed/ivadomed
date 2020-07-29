00e23b63a21cf9c1e2d0b86683c052865d3826d3
andreanne-lemay <andreanne.lemay@polymtl.ca>
2020-07-28 21:29:08 -0400

    use logdir instead of config

--- dev/reproducibility.py
+++ dev/reproducibility.py
@@ -12,7 +12,8 @@ from ivadomed import main as ivado
 
 def get_parser():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-l", "--log-directory", required=True, nargs="+", help="Log directory of trained model.")
+    parser.add_argument("-l", "--log-directory", dest="logdir", required=True, nargs="+",
+                        help="Log directory of trained model.")
     parser.add_argument("-b", "--bids-path", required=True, type=str, help="Bids path where are located the GT.")
     parser.add_argument("-n", "--iterations", default=10, type=int, help="Number of Monte Carlo iterations.")
     parser.add_argument("-o", "--output-path", nargs="+", dest="output_path", required=True,
@@ -33,7 +34,7 @@ def get_results(context):
     return ivado.run_command(context)
 
 
-def compute_csa(config, df_results):
+def compute_csa(config, df_results, logdir):
     subject_list = list(df_results.index)
     df_results = df_results.assign(csa_pred="", csa_gt="", absolute_csa_diff="", relative_csa_diff="")
     for subject in subject_list:
@@ -46,7 +47,7 @@ def compute_csa(config, df_results):
         os.system("rm csa.csv")
 
         # Get prediction csa
-        pred_path = os.path.join(config["log_directory"], "pred_masks", subject + "_pred.nii.gz")
+        pred_path = os.path.join(logdir, "pred_masks", subject + "_pred.nii.gz")
         pred_nii = nib.load(pred_path)
         # Keep only first label to compute csa
         single_label_pred = nib.Nifti1Image(pred_nii.get_fdata()[..., 0], pred_nii.affine)
@@ -73,7 +74,8 @@ def main():
     parser = get_parser()
     args = parser.parse_args()
 
-    for config, output_path in zip(args.config, args.output_path):
+    for logdir, output_path in zip(args.logdir, args.output_path):
+        config = os.path.join(logdir, "config_file.json")
         with open(config, "r") as fhandle:
             context = json.load(fhandle)
 
@@ -81,7 +83,7 @@ def main():
         metrics = []
         for i in range(int(args.iterations)):
             df = get_results(context)
-            df = compute_csa(context, df)
+            df = compute_csa(context, df, logdir)
             metrics = list(df.columns)
             df_list.append(np.array(df))
 
