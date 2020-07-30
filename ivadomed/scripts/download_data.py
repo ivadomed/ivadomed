ac2b7dfaf4c3e41c2c24dee93aeba897535e2417
andreanne-lemay <andreanne.lemay@polymtl.ca>
2020-07-30 11:55:32 -0400

    update data_exemple_spinegeneric url link

--- ivadomed/scripts/download_data.py
+++ ivadomed/scripts/download_data.py
@@ -204,7 +204,7 @@ def main(args=None):
     # If exists, favour release artifact straight from github
 
     dict_url = {
-        "data_example_spinegeneric": ["https://github.com/ivadomed/data_example_spinegeneric/archive/r20200907.zip"],
+        "data_example_spinegeneric": ["https://github.com/ivadomed/data_example_spinegeneric/archive/r20200730.zip"],
         "data-testing": ["https://github.com/ivadomed/data-testing/archive/master.zip"],
         "t2_tumor": ["https://github.com/ivadomed/t2_tumor/archive/r20200621.zip"],
         "t2star_sc": ["https://github.com/ivadomed/t2star_sc/archive/r20200622.zip"],
