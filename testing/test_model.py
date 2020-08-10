c7d40acc24ff16e400bd28033dc94e2adc99f345
lrouhier <lucas.rouhier@telecom-paristech.fr>
2020-08-10 14:21:20 -0400

    adding attention unet3d testing

--- testing/test_model.py
+++ testing/test_model.py
@@ -7,4 +7,14 @@ def test_countception():
     a = [[[[0 for i in range(10)] for i in range(10)]]]
     inp = torch.tensor(a).float()
     model = imed_model.Countception(in_channel=1, out_channel=1)
-    model(inp)
+    inf = model(inp)
+    assert (type(inf) == torch.Tensor)
+
+
+def test_model_3d_att():
+    # verifying if 3d attention model can be created
+    a = [[[[[0 for i in range(48)] for j in range(48)] for k in range(16)]]]
+    inp = torch.tensor(a).float()
+    model = imed_model.UNet3D(in_channel=1, out_channel=1, attention=True)
+    inf = model(inp)
+    assert(type(inf) == torch.Tensor)
