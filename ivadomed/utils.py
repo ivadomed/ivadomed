d36f76fb0e7435acbe243910b6fcdf6eee0840ab
andreanne-lemay <andreanne.lemay@polymtl.ca>
2020-08-11 17:24:57 -0400

    change matplotlib version for gif

--- ivadomed/utils.py
+++ ivadomed/utils.py
@@ -1014,7 +1014,7 @@ def overlap_im_seg(img, seg):
 class LoopingPillowWriter(anim.PillowWriter):
     def finish(self):
         self._frames[0].save(
-            self._outfile, save_all=True, append_images=self._frames[1:],
+            self.outfile, save_all=True, append_images=self._frames[1:],
             duration=int(1000 / self.fps), loop=0)
 
 
