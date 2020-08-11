dfbb7bbb693322a1b3a6733da605b82888b779b6
lrouhier <lucas.rouhier@telecom-paristech.fr>
2020-08-11 11:52:52 -0400

    using constrast. will need fixing before using the mri params can be tested

--- testing/test_script_FILM.py
+++ testing/test_script_FILM.py
@@ -78,7 +78,7 @@ def test_create_json_film():
 
     initial_config["FiLMedUnet"] = {
         "applied": True,
-        "metadata": "mri_params",
+        "metadata": "contrasts",
         "film_layers": [0, 1, 0, 0, 0, 0, 0, 0, 0, 0]
     }
 
