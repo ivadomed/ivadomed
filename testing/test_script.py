02f3747a7d005c0dc43402ffaa130bdecc9ac339
lrouhier <lucas.rouhier@telecom-paristech.fr>
2020-08-19 15:27:07 -0400

    adding test for multiple folder

--- testing/test_script.py
+++ testing/test_script.py
@@ -241,8 +241,13 @@ def test_testing_unet3d():
     subprocess.check_output(["ivadomed -c testing_data/model_config_3d.json"], shell=True)
 
 
-def test_training_curve():
+def test_training_curve_single():
     subprocess.check_output(["ivadomed_training_curve -i testing_script -o visu_test"], shell=True)
+
+
+def test_training_curve_multiple():
+    subprocess.check_output(["ivadomed_training_curve -i ./testing_script-batch_size= --multiple "
+                             "-o visu_test_multiple"], shell=True)
 
 
 def append_list_as_row(file_name, list_of_elem):
