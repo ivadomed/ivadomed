e879e0c38bdcefa156dddc7cf8e60083bef90a98
lrouhier <lucas.rouhier@telecom-paristech.fr>
2020-08-20 12:42:14 -0400

    error on type

--- testing/test_model.py
+++ testing/test_model.py
@@ -50,7 +50,7 @@ def test_film_generator():
     model = imed_model.FiLMgenerator(64, 1)
     inf = model(inp)
     assert (type(inf[0]) == torch.Tensor)
-    assert (type(inf[1]) == torch.Tensor)
+    assert (type(inf[1]) == torch.nn.parameter.Parameter)
 
 
 
