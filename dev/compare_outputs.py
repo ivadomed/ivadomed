c19dbef0d4add4ba4e8b864bfa129456d65f3202
Olivier Vincent <ovincent.poly@gmail.com>
2020-08-21 17:44:56 -0400

    bug fixes

--- dev/compare_outputs.py
+++ dev/compare_outputs.py
@@ -1,26 +1,37 @@
 from ivadomed import metrics as imed_metrics
 import argparse
 import os
-
+import nibabel
+import numpy as np
 def ignore(input):
-    return "soft" in input
+    return "soft" not in input
 
 def get_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument("-r", "--rater-agreement-path", dest='rpath', required=True, help="Path to folder containing.")
     parser.add_argument("-u", "--uncertainty-path", dest='upath', required=True, help="Path to pred_masks folder.")
-
+    return parser
+def main():
     parser = get_parser()
     args = parser.parse_args()
     rpath =  args.rpath
     upath =  args.upath
-    niftis = os.listdir(rpath)
+    niftis = os.listdir(upath)
     div = []
     for nifti in niftis:
+        #print(nifti)
         if not ignore(nifti):
-            im1 = nibabel.load(nifti).get_data()
-            fname = nifti.split("/")[-1]
-            im2 = nibabel.load(os.path.join(upath,fname)).get_data()
+            #print("not ignored")
+            print(nifti)
+            im1 = np.squeeze(nibabel.load(os.path.join(upath,nifti)).get_data())
+            fname = nifti
+            im2 = np.squeeze(nibabel.load(os.path.join(rpath,fname)).get_data())
             el = imed_metrics.js_divergence(im1,im2)
             print(el)
             div.append(el)
+
+
+
+if __name__ == '__main__':
+    main()
+
