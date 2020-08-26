9832bd6dc4b87d9ae559538251d38ab8f1660e00
lrouhier <lucas.rouhier@telecom-paristech.fr>
2020-08-26 15:20:59 -0400

    adding assert for ploting test

--- testing/test_metrics.py
+++ testing/test_metrics.py
@@ -1,6 +1,7 @@
 import ivadomed.metrics as imed_metrics
 import numpy as np
 import pytest
+import os
 
 
 @pytest.mark.parametrize("image", np.array([[[1, 1], [1, 1]], [[0, 0], [0, 0]]]))
@@ -21,6 +22,7 @@ def test_haussdorf_4d(image):
     assert results == 0.0
 
 
+# We test error case for the metrics
 @pytest.mark.parametrize("image", np.array([[[0, 0], [0, 0]], [[0, 0], [0, 0]]]))
 @pytest.mark.parametrize("image_2", np.array([[[0, 0], [0, 0]], [[0, 0], [0, 0]]]))
 def test_err_prec(image, image_2):
@@ -49,17 +51,20 @@ def test_err_iou(image, image_2):
     assert results == 12
 
 
+# we test wether the ploting code run or not
 def test_plot_roc_curve():
     tpr = [0, 0.1, 0.5, 0.6, 0.9]
     fpr = [1, 0.8, 0.5, 0.3, 0.1]
     opt_thr_idx = 3
     imed_metrics.plot_roc_curve(tpr, fpr, opt_thr_idx, "roc_test.png")
+    assert os.isfile("roc_test.png")
 
 
 def test_dice_plot():
     thr_list = [0.1, 0.3, 0.5, 0.7]
     dice_list = [0.6, 0.7, 0.8, 0.75]
     imed_metrics.plot_dice_thr(thr_list, dice_list, 2, "test_dice.png")
+    assert os.isfile("test_dice.png")
 
 
 
