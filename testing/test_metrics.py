3027d43601c248193d1c47ffcc6365bfd4ed9f7c
lrouhier <lucas.rouhier@telecom-paristech.fr>
2020-08-26 15:12:56 -0400

    adding pytest parametrize

--- testing/test_metrics.py
+++ testing/test_metrics.py
@@ -2,51 +2,45 @@ import ivadomed.metrics as imed_metrics
 import numpy as np
 
 
-def test_multi_class_dice_score():
-    # create fake image
-    image = np.array([[[1, 1], [1, 1]], [[0, 0], [0, 0]]])
+@pytest.mark.parametrize("image", np.array([[[1, 1], [1, 1]], [[0, 0], [0, 0]]]))
+def test_multi_class_dice_score(image):
     results = imed_metrics.multi_class_dice_score(image, image)
 
 
-def test_mse():
-    # create fake image
-    image = np.array([[[1, 1], [1, 1]], [[0, 0], [0, 0]]])
+@pytest.mark.parametrize("image", np.array([[[1, 1], [1, 1]], [[0, 0], [0, 0]]]))
+def test_mse(image):
     results = imed_metrics.mse(image, image)
 
 
-def test_haussdorf_4d():
-    # create fake image
-    image = np.array([[[[1, 1], [1, 1]], [[0, 0], [0, 0]]]])
+@pytest.mark.parametrize("image", np.array([[[[1, 1], [1, 1]], [[0, 0], [0, 0]]]]))
+def test_haussdorf_4d(image):
     results = imed_metrics.hausdorff_score(image, image)
 
 
-def test_err_prec():
-    # create fake image
-    image = np.array([[[0, 0], [0, 0]], [[0, 0], [0, 0]]])
-    image_2 = np.array([[[0, 0], [0, 0]], [[0, 0], [0, 0]]])
+@pytest.mark.parametrize("image", np.array([[[0, 0], [0, 0]], [[0, 0], [0, 0]]]))
+@pytest.mark.parametrize("image_2", np.array([[[0, 0], [0, 0]], [[0, 0], [0, 0]]]))
+def test_err_prec(image, image_2):
     results = imed_metrics.precision_score(image, image_2)
     assert results == 0.0
 
 
-def test_err_rec():
-    # create fake image
-    image = np.array([[[0, 0], [0, 0]], [[0, 0], [0, 0]]])
-    image_2 = np.array([[[0, 0], [0, 0]], [[0, 0], [0, 0]]])
+@pytest.mark.parametrize("image", np.array([[[0, 0], [0, 0]], [[0, 0], [0, 0]]]))
+@pytest.mark.parametrize("image_2", np.array([[[0, 0], [0, 0]], [[0, 0], [0, 0]]]))
+def test_err_rec(image, image_2):
     results = imed_metrics.recall_score(image, image_2, err_value=1)
     assert results == 1
 
 
-def test_err_spec():
-    # create fake image
-    image = np.array([[[1, 1], [1, 1]], [[1, 1], [1, 1]]])
-    image_2 = np.array([[[1, 1], [1, 1]], [[1, 1], [1, 1]]])
+@pytest.mark.parametrize("image", np.array([[[0, 0], [0, 0]], [[0, 0], [0, 0]]]))
+@pytest.mark.parametrize("image_2", np.array([[[0, 0], [0, 0]], [[0, 0], [0, 0]]]))
+def test_err_spec(image, image_2):
     results = imed_metrics.specificity_score(image, image_2, err_value=12)
     assert results == 12
 
 
-def test_err_iou():
-    image = np.array([[[0, 0], [0, 0]], [[0, 0], [0, 0]]])
-    image_2 = np.array([[[0, 0], [0, 0]], [[0, 0], [0, 0]]])
+@pytest.mark.parametrize("image", np.array([[[0, 0], [0, 0]], [[0, 0], [0, 0]]]))
+@pytest.mark.parametrize("image_2", np.array([[[0, 0], [0, 0]], [[0, 0], [0, 0]]]))
+def test_err_iou(image, image_2):
     results = imed_metrics.intersection_over_union(image, image_2, err_value=12)
     assert results == 12
 
