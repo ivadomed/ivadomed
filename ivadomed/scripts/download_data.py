931679074f6589e569e7284de4820425c57c3c03
lrouhier <lucas.rouhier@telecom-paristech.fr>
2020-09-04 14:38:46 -0400

    wrong type

--- ivadomed/scripts/download_data.py
+++ ivadomed/scripts/download_data.py
@@ -180,7 +180,7 @@ def install_data(url, dest_folder, keep=False):
             listing existing data bundle with their url. Type ivadomed_download_data -h to see possible value. Flag ``-d``
         dest_folder (string): destination directory for the data (to be created). If not used the output folder
             will be the name of the data bundle. Flag ``-o``, ``--output``
-        keep (string): whether to keep existing data in the destination folder (if it exists). Flag ``-k``, ``--keep``
+        keep (bool): whether to keep existing data in the destination folder (if it exists). Flag ``-k``, ``--keep``
     """
 
     if not keep and os.path.exists(dest_folder):
