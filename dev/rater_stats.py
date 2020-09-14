4a36230940a24be5c4e0d5f1daa988e46fed46cd
Olivier <oli_ch@hotmail.com>
2020-09-14 13:47:33 -0400

    Update rater_stats.py

--- dev/rater_stats.py
+++ dev/rater_stats.py
@@ -29,7 +29,7 @@ for subject in subjects:
             if rater.isnumeric():
                 fname = os.path.join(deriv_path,subject,"anat",nii)
                 im1 = nibabel.load(fname).get_data()
-                df.append(base_name, rater, "", 0)
+                df.append([base_name, rater, "", 0])
                 #print(base_name)
                 #print(rater)
 
