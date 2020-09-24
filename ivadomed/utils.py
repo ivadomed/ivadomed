fddbc16a4ad46fe508698f52845223155134386e
charleygros <charley.gros@gmail.com>
2020-09-24 14:50:56 +1000

    utils: __get_commit

--- ivadomed/utils.py
+++ ivadomed/utils.py
@@ -1,5 +1,6 @@
 import json
 import os
+import subprocess
 
 import matplotlib
 import matplotlib.animation as anim
@@ -1137,3 +1138,40 @@ def check_exe(name):
                 return exe_file
 
     return None
+
+
+def __get_commit(path_to_git_folder=None):
+    """
+    :return: git commit ID, with trailing '*' if modified
+    """
+    if path_to_git_folder is None:
+        path_to_git_folder = __ivadomed_dir__
+    else:
+        path_to_git_folder = os.path.abspath(os.path.expanduser(path_to_git_folder))
+
+    p = subprocess.Popen(["git", "rev-parse", "HEAD"], stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                         cwd=path_to_git_folder)
+    output, _ = p.communicate()
+    status = p.returncode
+    if status == 0:
+        commit = output.decode().strip()
+    else:
+        commit = "?!?"
+
+    p = subprocess.Popen(["git", "status", "--porcelain"], stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                         cwd=path_to_git_folder)
+    output, _ = p.communicate()
+    status = p.returncode
+    if status == 0:
+        unclean = True
+        for line in output.decode().strip().splitlines():
+            line = line.rstrip()
+            if line.startswith("??"):  # ignore ignored files, they can't hurt
+                continue
+            break
+        else:
+            unclean = False
+        if unclean:
+            commit += "*"
+
+    return commit
