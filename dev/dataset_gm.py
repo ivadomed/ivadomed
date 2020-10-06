d1e629035709a692244232fb65aae7dea12fa31d
Olivier <oli_ch@hotmail.com>
2020-10-06 11:44:21 -0400

    Update dataset_gm.py

--- dev/dataset_gm.py
+++ dev/dataset_gm.py
@@ -2,16 +2,16 @@ import os
 import random
 import shutil
 
-path = "~/duke/sct_testing/gmseg_challenge_16"
+path = "../duke/sct_testing/gmseg_challenge_16/"
 subfolders = [ name for name in os.listdir(path) if os.path.isdir(os.path.join(path, name)) ]
 
 for subfolder in subfolders:
 
     if "ucl" in subfolder:
-        sub_no = subfolder.split("_")[2]
+        sub_no = subfolder.split("_")[1]
         folder_name = "sub-ucl" + sub_no
     if "unf_pain" in subfolder:
-        sub_no = subfolder.split("_")[1]
+        sub_no = subfolder.split("_")[2]
         folder_name = "sub-unf" + sub_no
     elif "vanderbilt" in subfolder:
         sub_no = subfolder.split("_")[1]
