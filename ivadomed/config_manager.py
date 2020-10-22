34a2458f40288e0ec7ec6192027e5715ec5b3770
andreanne-lemay <andreanne.lemay@polymtl.ca>
2020-10-22 13:34:02 -0400

    config_manager.py: add validate path method

--- ivadomed/config_manager.py
+++ ivadomed/config_manager.py
@@ -1,5 +1,6 @@
-import os
 import json
+import os
+
 from ivadomed.utils import __ivadomed_dir__
 
 
@@ -11,6 +12,8 @@ def _load_json(config_path):
 
 class ConfigurationManager(object):
     def __init__(self, context_path):
+        self.context_path = context_path
+        self._validate_path()
         default_config_path = os.path.join(__ivadomed_dir__, "ivadomed", "config", "config.json")
         self.default_config = _load_json(default_config_path)
         self.context = _load_json(context_path)
@@ -18,3 +21,8 @@ class ConfigurationManager(object):
     def get_config(self):
         self.default_config.update(self.context)
         return self.default_config
+
+    def _validate_path(self):
+        if not os.path.isfile(self.context_path) or not self.context_path.endswith('.json'):
+            raise ValueError(
+                "\nERROR: The provided configuration file path (.json) is invalid: {}\n".format(self.context_path))
