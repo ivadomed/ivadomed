1041b4eab2a52f43068a70a03d5d0795b6dfce04
Olivier <oli_ch@hotmail.com>
2020-11-03 17:17:33 -0500

    Update rater_stats.py

--- dev/rater_stats.py
+++ dev/rater_stats.py
@@ -15,28 +15,7 @@ def subjectFilter(input):
         return False
 
 
-def compute_majority(dict):
-    new_dict = {}
-    for rater in dict:
-        hspace = 0.25
-        wspace = 0.25
-        dspace = 2
-        zooms = (dict[rater])[2]
-        hfactor = zooms[0] / hspace
-        wfactor = zooms[1] / wspace
-        dfactor = zooms[2] / dspace
-        params_resample = (hfactor, wfactor, dfactor)
-        # Run resampling
-        data_out = zoom((dict[rater])[1],
-                        zoom=params_resample,
-                        order=1)
 
-        print(data_out.shape)
-        crop = imed_transforms.CenterCrop([128, 128,15])
-        metadata = {}
-        metadata['crop_params'] = {}
-        print(crop(data_out,metadata)[0].shape)
-        new_dict[rater] = ((dict[rater])[0], crop(data_out,metadata)[0])
 #contrasts = ["FLAIR", "ce-T1w", "PD", "T1w", "T2w"]
 contrasts = ["T2star"]
 #ms brain
@@ -47,6 +26,17 @@ deriv_path = "../duke/projects/ivadomed/gm_challenge_16_inter_rater/derivatives/
 
 subjects=list(filter(subjectFilter,os.listdir(deriv_path)))
 print(subjects)
+
+def ms_brain_center_consensus(dict)
+    center_1 = dict["1"][2] + dict["2"][2] + dict["4"][2] + dict["5"][2]
+    center_1 = np.where(center_1 >= threshold, 1, 0)
+    center_2 = dict["3"][2] + dict["6"][2]
+    center_2 = np.where(center_2 >= threshold, 1, 0)
+    center_3 = dict["7"][2]
+    nibabel.save( nibabel.Nifti1Image(center_1, nibabel.load(dict["1"][0]).get_affine()), "_".join(fname.split("_")[0:-1]) + "majority-center1" + ".nii.gz")
+    nibabel.save( nibabel.Nifti1Image(center_2, nibabel.load(dict["3"][0]).get_affine()), "_".join(fname.split("_")[0:-1]) + "_majority-center2" + ".nii.gz")
+    nibabel.save( nibabel.Nifti1Image(center_3, nibabel.load(dict["7"][0]).get_affine()), "_".join(fname.split("_")[0:-1]) + "_majority-center3" + ".nii.gz")
+
 
 df = pd.DataFrame()
 df2 = pd.DataFrame()
@@ -70,35 +60,36 @@ for subject in subjects:
                 #print(zooms)
                 im1[im1 > 0] = 1
                 #im1[im1 < 0.5] = 0
-                dict[rater] = (base_name,im1,zooms)
+                dict[rater] = (fname, base_name, im1, zooms)
                 labels = measure.label(im1)
-                df = df.append({'file': base_name, 'rater': rater, 'lesion_count': labels.max(), 'positive_voxels': np.count_nonzero(im1)}, ignore_index=True)
+                df = df.append({'path': fname, 'file': base_name, 'rater': rater, 'lesion_count': labels.max(), 'positive_voxels': np.count_nonzero(im1)}, ignore_index=True)
                 #print(base_name)
                 #print(rater)
     #print(dict)
-    #compute_majority(dict)
+
+    ms_brain_center_consensus(dict)
 
     #Majority voting for gm
-    sum = np.zeros((dict["1"][1]).shape)
+    sum = np.zeros((dict["1"][2]).shape)
     for key in dict:
-        sum += dict[key][1]
+        sum += dict[key][2]
     threshold = 2
     im1 = np.where(sum >= threshold, 1, 0)
-    dict["0"] = (None, im1, None)
+    dict["0"] = (None, None, im1, None)
     labels = measure.label(im1)
     df = df.append({'file': "", 'rater': "0", 'lesion_count': labels.max(), 'positive_voxels': np.count_nonzero(im1)}, ignore_index=True)
 
-    gt = (dict["0"])[1]
+    gt = (dict["0"])[2]
     for key in dict:
         if key != "0":
-            im1 = (dict[key])[1]
+            im1 = (dict[key])[2]
             #Threshold since some files have 3 values [0, 0.2, 1]
             TP = np.count_nonzero(np.logical_and(im1, gt))
             FP = np.count_nonzero(np.logical_and(im1, np.logical_not(gt)))
             FN = np.count_nonzero(np.logical_and(np.logical_not(im1), gt))
             TN = np.count_nonzero(np.logical_and(np.logical_not(im1), np.logical_not(gt)))
             total = np.size(gt)
-            df2 = df2.append({'file': (dict[key])[0], 'rater': key, 'TP': TP/total, 'FP': FP/total, 'FN': FN/total, 'TN': TN/total}, ignore_index=True)
+            df2 = df2.append({'file': (dict[key])[1], 'rater': key, 'TP': TP/total, 'FP': FP/total, 'FN': FN/total, 'TN': TN/total}, ignore_index=True)
 
 print(df.head(30))
 df.to_csv('rater_lesion_stats.csv')
