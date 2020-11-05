5142b759938e7b41bc1355a2eec8f5dd17380905
lrouhier <lucas.rouhier@telecom-paristech.fr>
2020-11-05 00:35:36 -0500

    moved reorient image to loader/utils

--- ivadomed/loader/utils.py
+++ ivadomed/loader/utils.py
@@ -10,6 +10,7 @@ from bids_neuropoly import bids
 from sklearn.model_selection import train_test_split
 from torch._six import string_classes, int_classes
 from ivadomed import utils as imed_utils
+import nibabel as nib
 
 __numpy_type_map = {
     'float64': torch.DoubleTensor,
@@ -457,3 +458,27 @@ class SliceFilter(object):
                 return False
 
         return True
+
+
+
+def reorient_image(arr, slice_axis, nib_ref, nib_ref_canonical):
+    """Reorient an image to match a reference image orientation.
+
+    It reorients a array to a given orientation and convert it to a nibabel object using the reference nibabel header.
+
+    Args:
+        arr (ndarray): Input array, array to re orient.
+        slice_axis (int): Indicates the axis used for the 2D slice extraction: Sagittal: 0, Coronal: 1, Axial: 2.
+        nib_ref (nibabel): Reference nibabel object, whose header is used.
+        nib_ref_canonical (nibabel): `nib_ref` that has been reoriented to canonical orientation (RAS).
+    """
+    # Orient image in RAS according to slice axis
+    arr_ras = orient_img_ras(arr, slice_axis)
+
+    # https://gitship.com/neuroscience/nibabel/blob/master/nibabel/orientations.py
+    ref_orientation = nib.orientations.io_orientation(nib_ref.affine)
+    ras_orientation = nib.orientations.io_orientation(nib_ref_canonical.affine)
+    # Return the orientation that transforms from ras to ref_orientation
+    trans_orient = nib.orientations.ornt_transform(ras_orientation, ref_orientation)
+    # apply transformation
+    return nib.orientations.apply_orientation(arr_ras, trans_orient)
