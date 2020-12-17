4f203634201d1df66529e66d16dde01b57e9b18e
ahill187 <ainsleighhill@gmail.com>
2020-12-17 12:06:11 -0800

    download spinegeneric dataset for automate_training

--- testing/functional_tests/test_automate_training.py
+++ testing/functional_tests/test_automate_training.py
@@ -1,16 +1,17 @@
 import logging
 import os
-from cli_base import remove_tmp_dir, __tmp_dir__, create_tmp_dir
+from cli_base import remove_tmp_dir, __tmp_dir__, download_dataset, remove_dataset, create_tmp_dir
 from ivadomed.scripts import automate_training
 logger = logging.getLogger(__name__)
 
 
 def setup_function():
     create_tmp_dir()
+    download_dataset("data_example_spinegeneric")
 
 
 def test_automate_training():
-    config_file = os.path.join(__tmp_dir__, 'automate_training_config.json')
+    config_file = os.path.join(__tmp_dir__, 'automate_training_config_test.json')
     param_file = os.path.join(__tmp_dir__,
                               'automate_training_hyperparameter_opt.json')
     automate_training.main(args=[
@@ -20,4 +21,5 @@ def test_automate_training():
 
 
 def teardown_function():
+    remove_dataset("data_example_spinegeneric")
     remove_tmp_dir()
