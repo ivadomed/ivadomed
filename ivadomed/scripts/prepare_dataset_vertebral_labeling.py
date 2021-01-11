9a26412cb78f0f9c23174dc3ae45c9258d57a53e
Ainsleigh Hill <ainsleighhill@gmail.com>
2021-01-11 11:48:41 -0800

    add test for prepare_dataset_vertebral_labelling, code cleanup

--- ivadomed/scripts/prepare_dataset_vertebral_labeling.py
+++ ivadomed/scripts/prepare_dataset_vertebral_labeling.py
@@ -9,12 +9,12 @@ import ivadomed.loader.utils as imed_loader_utils
 
 
 def mask2label(path_label, aim=0):
-    """
-    Retrieve points coordinates and value from a label file containing singl voxel label
+    """Retrieve points coordinates and value from a label file containing singl voxel label.
+
     Args:
         path_label (str): path of nifti image
-        aim (int): -1 will return all points with label between 3 and 30 , any other int > 0  will return
-        only the coordinates of points with label defined by aim.
+        aim (int): -1 will return all points with label between 3 and 30 , any other int > 0
+            will return only the coordinates of points with label defined by aim.
 
     Returns:
         ndarray: array containing the asked point in the format [x,y,z,value] in the RAS orientation.
@@ -52,10 +52,12 @@ def extract_mid_slice_and_convert_coordinates_to_heatmaps(path, suffix, aim=-1):
         ivadomed_prepare_dataset_vertebral_labeling -p path/to/bids -s _T2w -a 0
 
     Args:
-        path (string): path to BIDS dataset form which images will be generated. Flag: ``--path``, ``-p``
-        suffix (string): suffix of image that will be processed (e.g., T2w). Flag: ``--suffix``, ``-s``
-        aim (int): If aim is not 0, retrieves only labels with value = aim, else create heatmap with all labels.
-                  Flag: ``--aim``, ``-a``
+        path (string): path to BIDS dataset form which images will be generated.
+            Flag: ``--path``, ``-p``
+        suffix (string): suffix of image that will be processed (e.g., T2w).
+            Flag: ``--suffix``, ``-s``
+        aim (int): If aim is not 0, retrieves only labels with value = aim, else create heatmap
+            with all labels. Flag: ``--aim``, ``-a``
 
     Returns:
         None. Images are saved in BIDS folder
@@ -107,10 +109,8 @@ def main(args=None):
     imed_utils.init_ivadomed()
     parser = get_parser()
     args = imed_utils.get_arguments(parser, args)
-    bids_path = args.path
-    suffix = args.suffix
-    aim = args.aim
-    extract_mid_slice_and_convert_coordinates_to_heatmaps(bids_path, suffix, aim)
+    extract_mid_slice_and_convert_coordinates_to_heatmaps(path=args.path, suffix=args.suffix,
+                                                          aim=args.aim)
 
 if __name__=='__main__':
     main()
--- testing/functional_tests/test_prepare_dataset_vertebral_labelling.py
+++ testing/functional_tests/test_prepare_dataset_vertebral_labelling.py
@@ -1,17 +1,26 @@
 import logging
 import os
-from cli_base import remove_tmp_dir, __tmp_dir__, create_tmp_dir
+from cli_base import remove_tmp_dir, __tmp_dir__, create_tmp_dir, download_dataset
 from ivadomed.scripts import prepare_dataset_vertebral_labeling
 logger = logging.getLogger(__name__)
 
 
 def setup_function():
     create_tmp_dir()
+    download_dataset("data_functional_testing")
 
 
 def test_prepare_dataset_vertebral_labeling():
-    # prepare_dataset_vertebral_labeling.main(args=[])
-    assert 1 == 1
+    __input_dir__ = os.path.join(__tmp_dir__, 'data_functional_testing')
+    prepare_dataset_vertebral_labeling.main(args=['--path', __input_dir__,
+                                                  '--suffix', '_T2w',
+                                                  '--aim', '3'])
+    assert os.path.exists(os.path.join(
+        __input_dir__, "derivatives/labels/sub-unf01/anat/sub-unf01_T2w_mid_heatmap3.nii.gz"))
+    assert os.path.exists(os.path.join(
+        __input_dir__, "derivatives/labels/sub-unf02/anat/sub-unf02_T2w_mid_heatmap3.nii.gz"))
+    assert os.path.exists(os.path.join(
+        __input_dir__, "derivatives/labels/sub-unf03/anat/sub-unf03_T2w_mid_heatmap3.nii.gz"))
 
 
 def teardown_function():
