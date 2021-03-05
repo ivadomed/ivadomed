43c0b50636cd48bab254dfffe13d9105c800aab2
Marie-Helene Bourget <bourgetmarieh@gmail.com>
2021-03-05 17:17:13 -0500

    Update test_loader.py

--- testing/unit_tests/test_loader.py
+++ testing/unit_tests/test_loader.py
@@ -2,7 +2,7 @@ import os
 import pytest
 import csv_diff
 import logging
-from unit_tests.t_utils import remove_tmp_dir, create_tmp_dir, __data_testing_dir__
+from unit_tests.t_utils import remove_tmp_dir, create_tmp_dir, __data_testing_dir__, __tmp_dir__
 from ivadomed.loader import utils as imed_loader_utils
 logger = logging.getLogger(__name__)
 
@@ -16,17 +16,10 @@ def setup_function():
     "bids_config": "ivadomed/config/config_bids.json",
     "target_suffix": [["_seg-myelin-manual", "_seg-axon-manual"]],
     "extensions": [".png"],
-    "roi_params": {
-        "suffix": None,
-        "slice_filter_roi": None
-    },
-    "contrast_params": {
-        "training_validation": [],
-        "testing": [],
-        "balance": {}
-    }}])
-@pytest.mark.parametrize('path_output', [os.path.join(__data_testing_dir__, "microscopy_png", "log_directory")])
-def test_bids_df_microscopy_png(loader_parameters, path_output):
+    "roi_params": {"suffix": None, "slice_filter_roi": None},
+    "contrast_params": {"contrast_lst": []}
+    }])
+def test_bids_df_microscopy_png(loader_parameters):
     """
     Test for microscopy png file format
     Test for _sessions.tsv and _scans.tsv files
@@ -34,18 +27,14 @@ def test_bids_df_microscopy_png(loader_parameters, path_output):
     Test for when no contrast_params are provided
     """
 
-    loader_params = loader_parameters
-    loader_params["contrast_params"]["contrast_lst"] = loader_params["contrast_params"]["training_validation"]
-    path_data = loader_params["path_data"]
-    bids_df = imed_loader_utils.BidsDataframe(loader_params, path_output, derivatives = True)
-    df_test = bids_df.df
-    df_test = df_test.drop(columns=['path'])
+    bids_df = imed_loader_utils.BidsDataframe(loader_parameters, __tmp_dir__, derivatives=True)
+    df_test = bids_df.df.drop(columns=['path'])
     # TODO: modify df_ref.csv file in data-testing dataset to include "participant_id"
     # and "sample_id" columns, then delete next line
     df_test = df_test.drop(columns=['participant_id', 'sample_id'])
     df_test = df_test.sort_values(by=['filename']).reset_index(drop=True)
-    csv_ref = os.path.join(path_data[0], "df_ref.csv")
-    csv_test = os.path.join(path_data[0], "df_test.csv")
+    csv_ref = os.path.join(loader_parameters["path_data"][0], "df_ref.csv")
+    csv_test = os.path.join(loader_parameters["path_data"][0], "df_test.csv")
     df_test.to_csv(csv_test, index=False)
     diff = csv_diff.compare(csv_diff.load_csv(open(csv_ref)), csv_diff.load_csv(open(csv_test)))
     assert diff == {'added': [], 'removed': [], 'changed': [], 'columns_added': [], 'columns_removed': []}
@@ -55,17 +44,10 @@ def test_bids_df_microscopy_png(loader_parameters, path_output):
     "path_data": [__data_testing_dir__],
     "target_suffix": ["_seg-manual"],
     "extensions": [],
-    "roi_params": {
-        "suffix": None,
-        "slice_filter_roi": None
-    },
-    "contrast_params": {
-        "training_validation": ["T1w", "T2w"],
-        "testing": [],
-        "balance": {}
-    }}])
-@pytest.mark.parametrize('path_output', [os.path.join(__data_testing_dir__, "log_directory")])
-def test_bids_df_anat(loader_parameters, path_output):
+    "roi_params": {"suffix": None, "slice_filter_roi": None},
+    "contrast_params": {"contrast_lst": ["T1w", "T2w"]}
+    }])
+def test_bids_df_anat(loader_parameters):
     """
     Test for MRI anat nii.gz file format
     Test for when no file extensions are provided
@@ -73,18 +55,14 @@ def test_bids_df_anat(loader_parameters, path_output):
     TODO: modify test and "df_ref.csv" file in data-testing dataset to test behavior when "roi_suffix" is not None
     """
 
-    loader_params = loader_parameters
-    loader_params["contrast_params"]["contrast_lst"] = loader_params["contrast_params"]["training_validation"]
-    path_data = loader_params["path_data"]
-    bids_df = imed_loader_utils.BidsDataframe(loader_params, path_output, derivatives = True)
-    df_test = bids_df.df
-    df_test = df_test.drop(columns=['path'])
+    bids_df = imed_loader_utils.BidsDataframe(loader_parameters, __tmp_dir__, derivatives = True)
+    df_test = bids_df.df.drop(columns=['path'])
     # TODO: modify df_ref.csv file in data-testing dataset to include "participant_id"
     # column then delete next line
     df_test = df_test.drop(columns=['participant_id'])
     df_test = df_test.sort_values(by=['filename']).reset_index(drop=True)
-    csv_ref = os.path.join(path_data[0], "df_ref.csv")
-    csv_test = os.path.join(path_data[0], "df_test.csv")
+    csv_ref = os.path.join(loader_parameters["path_data"][0], "df_ref.csv")
+    csv_test = os.path.join(loader_parameters["path_data"][0], "df_test.csv")
     df_test.to_csv(csv_test, index=False)
     diff = csv_diff.compare(csv_diff.load_csv(open(csv_ref)), csv_diff.load_csv(open(csv_test)))
     assert diff == {'added': [], 'removed': [], 'changed': [],
