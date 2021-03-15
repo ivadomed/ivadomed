e6cd354cd404ccd4cd9a7a654212b46cfce4123c
Marie-Helene Bourget <bourgetmarieh@gmail.com>
2021-03-15 18:55:14 -0400

    test_loader.py: Update test_bids_df_anat

--- testing/unit_tests/test_loader.py
+++ testing/unit_tests/test_loader.py
@@ -39,9 +39,9 @@ def test_bids_df_microscopy_png(loader_parameters):
 
 @pytest.mark.parametrize('loader_parameters', [{
     "path_data": [__data_testing_dir__],
-    "target_suffix": ["_seg-manual"],
+    "target_suffix": ["_lesion-manual"],
     "extensions": [],
-    "roi_params": {"suffix": None, "slice_filter_roi": None},
+    "roi_params": {"suffix": "_seg-manual", "slice_filter_roi": None},
     "contrast_params": {"contrast_lst": ["T1w", "T2w"]}
     }])
 def test_bids_df_anat(loader_parameters):
@@ -49,14 +49,11 @@ def test_bids_df_anat(loader_parameters):
     Test for MRI anat nii.gz file format
     Test for when no file extensions are provided
     Test for multiple target_suffix
-    TODO: modify test and "df_ref.csv" file in data-testing dataset to test behavior when "roi_suffix" is not None
+    Test behavior when "roi_suffix" is not None
     """
 
     bids_df = imed_loader_utils.BidsDataframe(loader_parameters, __tmp_dir__, derivatives = True)
     df_test = bids_df.df.drop(columns=['path'])
-    # TODO: modify df_ref.csv file in data-testing dataset to include "participant_id"
-    # column then delete next line
-    df_test = df_test.drop(columns=['participant_id'])
     df_test = df_test.sort_values(by=['filename']).reset_index(drop=True)
     csv_ref = os.path.join(loader_parameters["path_data"][0], "df_ref.csv")
     csv_test = os.path.join(loader_parameters["path_data"][0], "df_test.csv")
