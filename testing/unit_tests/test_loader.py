d9a5bdb652dd79e588dcf7988b7fae8e6ecf8be8
mariehbourget <54086142+mariehbourget@users.noreply.github.com>
2021-03-24 17:05:50 -0400

    New loader: Update test_loader.py (#725)
    * BidsDataframe: insert filename in second column
    
    * test_loader.py: Update test_bids_df_microscopy_png
    
    * test_loader.py: Update test_bids_df_anat
    
    * test_loader.py: Add test for multiple folder in path_data
    
    * test_loader.py: Add test for ct-scan indexing
    
    * Update comments test_loader.py + PEP8
    
    * Correction PEP8
    
    * Update data-testing link for download

--- testing/unit_tests/test_loader.py
+++ testing/unit_tests/test_loader.py
@@ -31,9 +31,6 @@ def test_bids_df_microscopy_png(loader_parameters):
 
     bids_df = imed_loader_utils.BidsDataframe(loader_parameters, __tmp_dir__, derivatives=True)
     df_test = bids_df.df.drop(columns=['path'])
-    # TODO: modify df_ref.csv file in data-testing dataset to include "participant_id"
-    # and "sample_id" columns, then delete next line
-    df_test = df_test.drop(columns=['participant_id', 'sample_id'])
     df_test = df_test.sort_values(by=['filename']).reset_index(drop=True)
     csv_ref = os.path.join(loader_parameters["path_data"][0], "df_ref.csv")
     csv_test = os.path.join(loader_parameters["path_data"][0], "df_test.csv")
@@ -44,9 +41,9 @@ def test_bids_df_microscopy_png(loader_parameters):
 
 @pytest.mark.parametrize('loader_parameters', [{
     "path_data": [__data_testing_dir__],
-    "target_suffix": ["_seg-manual"],
+    "target_suffix": ["_lesion-manual"],
     "extensions": [],
-    "roi_params": {"suffix": None, "slice_filter_roi": None},
+    "roi_params": {"suffix": "_seg-manual", "slice_filter_roi": None},
     "contrast_params": {"contrast_lst": ["T1w", "T2w"]}
     }])
 def test_bids_df_anat(loader_parameters):
@@ -54,14 +51,11 @@ def test_bids_df_anat(loader_parameters):
     Test for MRI anat nii.gz file format
     Test for when no file extensions are provided
     Test for multiple target_suffix
-    TODO: modify test and "df_ref.csv" file in data-testing dataset to test behavior when "roi_suffix" is not None
+    Test behavior when "roi_suffix" is not None
     """
 
-    bids_df = imed_loader_utils.BidsDataframe(loader_parameters, __tmp_dir__, derivatives = True)
+    bids_df = imed_loader_utils.BidsDataframe(loader_parameters, __tmp_dir__, derivatives=True)
     df_test = bids_df.df.drop(columns=['path'])
-    # TODO: modify df_ref.csv file in data-testing dataset to include "participant_id"
-    # column then delete next line
-    df_test = df_test.drop(columns=['participant_id'])
     df_test = df_test.sort_values(by=['filename']).reset_index(drop=True)
     csv_ref = os.path.join(loader_parameters["path_data"][0], "df_ref.csv")
     csv_test = os.path.join(loader_parameters["path_data"][0], "df_test.csv")
@@ -71,7 +65,53 @@ def test_bids_df_anat(loader_parameters):
                     'columns_added': [], 'columns_removed': []}
 
 
-# TODO: add a test to ensure the loader can read in multiple entries in path_data
+@pytest.mark.parametrize('loader_parameters', [{
+    "path_data": [__data_testing_dir__, os.path.join(__data_testing_dir__, "microscopy_png")],
+    "bids_config": "ivadomed/config/config_bids.json",
+    "target_suffix": ["_seg-manual", "seg-axon-manual"],
+    "extensions": [".nii.gz", ".png"],
+    "roi_params": {"suffix": None, "slice_filter_roi": None},
+    "contrast_params": {"contrast_lst": ["T1w", "T2w", "SEM"]}
+    }])
+def test_bids_df_multi(loader_parameters):
+    """
+    Test for multiple folders in path_data
+    """
+
+    bids_df = imed_loader_utils.BidsDataframe(loader_parameters, __tmp_dir__, derivatives=True)
+    df_test = bids_df.df.drop(columns=['path'])
+    df_test = df_test.sort_values(by=['filename']).reset_index(drop=True)
+    csv_ref = os.path.join(loader_parameters["path_data"][0], "df_ref_multi.csv")
+    csv_test = os.path.join(loader_parameters["path_data"][0], "df_test_multi.csv")
+    df_test.to_csv(csv_test, index=False)
+    diff = csv_diff.compare(csv_diff.load_csv(open(csv_ref)), csv_diff.load_csv(open(csv_test)))
+    assert diff == {'added': [], 'removed': [], 'changed': [],
+                    'columns_added': [], 'columns_removed': []}
+
+
+@pytest.mark.parametrize('loader_parameters', [{
+    "path_data": [os.path.join(__data_testing_dir__, "ct_scan")],
+    "bids_config": "ivadomed/config/config_bids.json",
+    "target_suffix": ["_seg-manual"],
+    "extensions": [".nii.gz"],
+    "roi_params": {"suffix": None, "slice_filter_roi": None},
+    "contrast_params": {"contrast_lst": ["ct"]}
+    }])
+def test_bids_df_ctscan(loader_parameters):
+    """
+    Test for ct-scan nii.gz file format
+    Test for when dataset_description.json is not present in derivatives folder
+    """
+
+    bids_df = imed_loader_utils.BidsDataframe(loader_parameters, __tmp_dir__, derivatives=True)
+    df_test = bids_df.df.drop(columns=['path'])
+    df_test = df_test.sort_values(by=['filename']).reset_index(drop=True)
+    csv_ref = os.path.join(loader_parameters["path_data"][0], "df_ref.csv")
+    csv_test = os.path.join(loader_parameters["path_data"][0], "df_test.csv")
+    df_test.to_csv(csv_test, index=False)
+    diff = csv_diff.compare(csv_diff.load_csv(open(csv_ref)), csv_diff.load_csv(open(csv_test)))
+    assert diff == {'added': [], 'removed': [], 'changed': [], 'columns_added': [], 'columns_removed': []}
+
 
 @pytest.mark.parametrize('seg_pair', [
     {"input": torch.rand((2, 5, 5))},
