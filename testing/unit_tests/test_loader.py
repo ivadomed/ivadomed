648c7276121fdc2870e56005ddbb06ae2c7eb1f5
Giselle Martel <gmartel90@gmail.com>
2021-05-13 14:36:22 -0400

    Add unit test for get_target_filename for list of target_suffix

--- testing/unit_tests/test_loader.py
+++ testing/unit_tests/test_loader.py
@@ -216,5 +216,51 @@ def test_2d_patches(download_data_testing_test_files,
     assert len(ds) == 16
 
 
+@pytest.mark.parametrize('loader_parameters', [{
+    "path_data": [os.path.join(__data_testing_dir__, "microscopy_png")],
+    "bids_config": f"{path_repo_root}/ivadomed/config/config_bids.json",
+    "target_suffix": ["_seg-myelin-manual"],
+    "extensions": [".png"],
+    "roi_params": {"suffix": None, "slice_filter_roi": None},
+    "contrast_params": {"contrast_lst": [], "balance": {}},
+    "slice_axis": "axial",
+    "slice_filter_params": {"filter_empty_mask": False, "filter_empty_input": True},
+    "multichannel": False
+    }])
+@pytest.mark.parametrize('model_parameters', [{
+    "name": "Unet",
+    "dropout_rate": 0.3,
+    "bn_momentum": 0.1,
+    "depth": 2
+    }])
+@pytest.mark.parametrize('transform_parameters', [{
+    "NumpyToTensor": {},
+    }])
+def test_get_target_filename_list(loader_parameters, model_parameters, transform_parameters):
+    """
+    Test that all target_suffix are considered for target filename when list
+    """
+    loader_parameters.update({"model_params": model_parameters})
+    bids_df = imed_loader_utils.BidsDataframe(loader_parameters, __tmp_dir__, derivatives=True)
+    data_lst = ['sub-rat3_ses-01_sample-data9_SEM.png']
+    test_ds = imed_loader.load_dataset(bids_df,
+                                  **{**loader_parameters, **{'data_list': data_lst,
+                                                             'transforms_params': transform_parameters,
+                                                             'dataset_type': 'training'}})
+
+    test_subject = 'sub-unf02_T2star.nii.gz'
+
+    all_deriv = bids_df.get_deriv_fnames()
+    test_derivatives = bids_df.df[bids_df.df['filename']
+                .str.contains('|'.join(bids_df.get_derivatives(test_subject, all_deriv)))]['path'].to_list()
+
+    target_filename = [None]
+
+    for test_deriv in test_derivatives:
+        target_filename = test_ds.get_target_filename(loader_parameters["target_suffix"], target_filename, test_deriv)
+    
+    assert len(target_filename) == len(loader_parameters["target_suffix"])
+
+
 def teardown_function():
     remove_tmp_dir()
