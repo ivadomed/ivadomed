1d9a8ca8949ebe2ae760b9705d77f5616d945cde
hanna <hanna.ma@motioncorrect.com>
2021-06-18 18:25:12 +0100

    fix test

--- testing/functional_tests/test_cascaded_architecture_tutorial.py
+++ testing/functional_tests/test_cascaded_architecture_tutorial.py
@@ -2,13 +2,13 @@ import logging
 import os
 import pytest
 from pytest_console_scripts import script_runner
-from testing.functional_tests.t_utils import __data_testing_dir__
+from testing.functional_tests.t_utils import __data_testing_dir__, download_functional_test_files
 from testing.common_testing_util import download_dataset, path_repo_root
 
 logger = logging.getLogger(__name__)
 
 @pytest.mark.script_launch_mode('subprocess')
-def test_cascaded_architecture_tutorial(script_runner):
+def test_cascaded_architecture_tutorial(download_functional_test_files, script_runner):
     download_dataset("data_example_spinegeneric")
     file_config = os.path.join(__data_testing_dir__, 'cascaded_architecture_tutorial_config.json')
 
