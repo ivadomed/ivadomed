2601274eee1ef090e6684522cd9aaa5be72f3276
Reza Azad <rezazad68@gmail.com>
2021-06-25 21:18:02 +0200

    Empty lines are removed

--- ivadomed/loader/loader.py
+++ ivadomed/loader/loader.py
@@ -764,8 +764,6 @@ class MRI2DSegmentationDataset(Dataset):
             if stack_gt is not None and not self.soft_gt:
                 stack_gt = imed_postpro.threshold_predictions(stack_gt, thr=0.5).astype(np.uint8)
                 
-
-
         else:
             # Force no transformation on labels for classification task
             # stack_gt is a tensor of size 1x1, values: 0 or 1
