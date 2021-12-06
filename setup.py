6a35316ddacff60f73ae385c32fc056c13630049
Kanishk <36276423+kanishk16@users.noreply.github.com>
2021-12-07 00:04:57 +0530

    fix: unify current installation

--- setup.py
+++ setup.py
@@ -1,21 +1,72 @@
-from setuptools import setup, find_packages
 from codecs import open
+# TODO: use pathlib
 from os import path
+from setuptools import setup, find_packages
+from distutils.core import Command
+import subprocess
+from sys import platform
+import shlex
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from README.md file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+# Get release version
+with open(path.join(here, 'ivadomed', 'version.txt')) as f:
+    version = f.read().strip()
 
 # Manually specified, more generic version of the software.
 # See: https://stackoverflow.com/a/49684835
-with open('requirements_common.txt') as f:
-    requirements = f.readlines()
+with open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
+    requirements = [line.strip() for line in f if line]
 
-# Get README
-this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
+class InstallTorch(Command):
+    description = "Installs the PyTorch utilities"
+    user_options = [
+        ('backend=', '0', '1'),
+    ]
+    
+    def initialize_options(self):
+        self.backend = '0'
+    
+    def finalize_options(self):
+        assert self.backend in ('0', '1'), 'Invalid backend!'
+        
+    def run(self):
+        if self.backend == '0' and platform == 'darwin':
+            command = f'pip install torch==1.8.0 torchvision==0.9.0 -f https://download.pytorch.org/whl/torch_stable.html'
+        elif self.backend == '0' and platform != 'darwin':
+            command = f'pip install torch==1.8.0+cpu torchvision==0.9.0+cpu -f https://download.pytorch.org/whl/torch_stable.html'
+        elif self.backend == '1':
+            command = f'pip install torch==1.8.1+cu111 torchvision==0.9.1+cu111 -f https://download.pytorch.org/whl/torch_stable.html'
+        
+        # TODO: inside try except
+        subprocess.run(shlex.split(command))
 
-# Get Release version
-path_version = path.join(this_directory, 'ivadomed', 'version.txt')
-with open(path_version) as f:
-    version = f.read().strip()
+extra_requirements = {}
+
+extra_requirements['dev'] = sorted(
+        'pytest~=6.2',
+        'pytest-cov',
+        'pytest-ordering~=0.6',
+        'sphinx',
+        'flake8',
+        'coverage',
+        'coveralls',
+        'pypandoc',
+        'sphinx_rtd_theme',
+        'sphinx-jsonschema~=1.16',
+        'pytest-console-scripts~=1.1',
+        'pre-commit>=2.10.1',
+        'sphinx-tabs==3.2.0'
+)
+# {
+#         'docs': [  # pin sphinx to match what RTD uses:
+#             # https://github.com/readthedocs/readthedocs.org/blob/ecac31de54bbb2c100f933e86eb22b0f4389ba84/requirements/pip.txt#L16
+#             'sphinx==4.2.0',
+#             'sphinx-rtd-theme<0.5',
 
 
 setup(
@@ -36,13 +87,9 @@ setup(
     packages=find_packages(exclude=['docs', 'tests']),
     include_package_data=True,
     install_requires=requirements,
-    extras_require={
-        'docs': [  # pin sphinx to match what RTD uses:
-            # https://github.com/readthedocs/readthedocs.org/blob/ecac31de54bbb2c100f933e86eb22b0f4389ba84/requirements/pip.txt#L16
-            'sphinx==4.2.0',
-            'sphinx-rtd-theme<0.5',
-        ],
-        'dev': ["pre-commit>=2.10.0"]
+    extras_require=extra_requirements,
+    cmdclass={
+        'install': InstallTorch,
     },
     entry_points={
         'console_scripts': [
