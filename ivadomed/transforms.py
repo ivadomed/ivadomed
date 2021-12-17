99b3dfc38c29b80f4982631f995defa7377a99d3
Uzay Macar <oum2000@columbia.edu>
2021-12-17 06:00:11 +0300

    Implemented random gamma contrast augmentation.

--- ivadomed/transforms.py
+++ ivadomed/transforms.py
@@ -1012,6 +1012,44 @@ class HistogramClipping(ImedTransform):
         return data, metadata
 
 
+class RandomGamma(ImedTransform):
+    """Randomly changes the contrast of an image by gamma exponential
+
+    Args:
+        log_gamma_range (tuple of floats): Log gamma range for changing contrast. Length equals 2.
+        p (float): Probability of performing the gamma contrast
+    """
+
+    def __init__(self, log_gamma_range, p=0.5):
+        self.log_gamma_range = log_gamma_range
+        self.p = p
+
+    @multichannel_capable
+    @two_dim_compatible
+    def __call__(self, sample, metadata=None):
+        if np.random.random() < self.p:
+            # Get params
+            gamma = np.exp(np.random.uniform(self.log_gamma_range[0], self.log_gamma_range[1]))
+
+            # Save params
+            metadata[MetadataKW.GAMMA] = [gamma]
+
+        else:
+            metadata[MetadataKW.GAMMA] = [None]
+
+        if any(metadata[MetadataKW.GAMMA]):
+            # Apply gamma contrast
+            data_out = np.sign(sample) * (np.abs(sample) ** gamma)
+
+            # Keep data type
+            data_out = data_out.astype(sample.dtype)
+
+            return data_out, metadata
+
+        else:
+            return sample, metadata
+
+
 def get_subdatasets_transforms(transform_params):
     """Get transformation parameters for each subdataset: training, validation and testing.
 
