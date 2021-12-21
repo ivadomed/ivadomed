c4ffd2717d1243df8f813831922d0553b1793f6e
Marie-Helene Bourget <bourgetmarieh@gmail.com>
2021-12-21 17:59:43 -0500

    mri2d_segmentation_dataset.py: Add patch_filter_fn

--- ivadomed/loader/mri2d_segmentation_dataset.py
+++ ivadomed/loader/mri2d_segmentation_dataset.py
@@ -25,7 +25,8 @@ class MRI2DSegmentationDataset(Dataset):
             "axial": 2, "sagittal": 0, "coronal": 1. 2D PNG/TIF/JPG files use default "axial": 2.
         cache (bool): if the data should be cached in memory or not.
         transform (torchvision.Compose): transformations to apply.
-        slice_filter_fn (dict): Slice filter parameters, see :doc:`configuration_file` for more details.
+        slice_filter_fn (SliceFilter): SliceFilter object containing Slice filter parameters.
+        patch_filter_fn (PatchFilter): PatchFilter object containing Patch filter parameters.
         task (str): choice between segmentation or classification. If classification: GT is discrete values, \
             If segmentation: GT is binary mask.
         roi_params (dict): Dictionary containing parameters related to ROI image processing.
@@ -46,7 +47,8 @@ class MRI2DSegmentationDataset(Dataset):
         cache (bool): Tf the data should be cached in memory or not.
         slice_axis (int): Indicates the axis used to extract 2D slices from 3D NifTI files:
             "axial": 2, "sagittal": 0, "coronal": 1. 2D PNG/TIF/JPG files use default "axial": 2.
-        slice_filter_fn (dict): Slice filter parameters, see :doc:`configuration_file` for more details.
+        slice_filter_fn (SliceFilter): SliceFilter object containing Slice filter parameters.
+        patch_filter_fn (PatchFilter): PatchFilter object containing Patch filter parameters.
         n_contrasts (int): Number of input contrasts.
         has_bounding_box (bool): True if bounding box in all metadata, else False.
         task (str): Choice between segmentation or classification. If classification: GT is discrete values, \
@@ -61,7 +63,8 @@ class MRI2DSegmentationDataset(Dataset):
     """
 
     def __init__(self, filename_pairs, length=None, stride=None, slice_axis=2, cache=True, transform=None,
-                 slice_filter_fn=None, task="segmentation", roi_params=None, soft_gt=False, is_input_dropout=False):
+                 slice_filter_fn=None, patch_filter_fn=None, task="segmentation", roi_params=None, soft_gt=False,
+                 is_input_dropout=False):
         if length is None:
             length = []
         if stride is None:
@@ -76,6 +79,7 @@ class MRI2DSegmentationDataset(Dataset):
         self.cache = cache
         self.slice_axis = slice_axis
         self.slice_filter_fn = slice_filter_fn
+        self.patch_filter_fn = patch_filter_fn
         self.n_contrasts = len(self.filename_pairs[0][0])
         if roi_params is None:
             roi_params = {ROIParamsKW.SUFFIX: None, ROIParamsKW.SLICE_FILTER_ROI: None}
@@ -156,12 +160,13 @@ class MRI2DSegmentationDataset(Dataset):
                         y_min = (shape[1] - self.length[1])
                     y_max = y_min + self.length[1]
 
+                    # Extract patch from handlers for patch filter
                     patch = {'input': list(np.asarray(self.handlers[i][0]['input'])[:, x_min:x_max, y_min:y_max]),
                              'gt': list(np.asarray(self.handlers[i][0]['gt'])[:, x_min:x_max, y_min:y_max]) \
                                    if self.handlers[i][0]['gt'] else [],
                              'input_metadata': self.handlers[i][0]['input_metadata'],
                              'gt_metadata': self.handlers[i][0]['gt_metadata']}
-                    if self.slice_filter_fn and not self.slice_filter_fn(patch, is_2d_patch=self.is_2d_patch):
+                    if self.slice_filter_fn and not self.patch_filter_fn(patch):
                         continue
 
                     self.indexes.append({
