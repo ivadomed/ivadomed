5df42ddc270e7e4a16f87fc5802657a3d32f9e5a
Uzay Macar <oum2000@columbia.edu>
2021-12-24 06:42:27 +0300

    Implemented random bias field augmentation.

--- ivadomed/transforms.py
+++ ivadomed/transforms.py
@@ -1056,6 +1056,51 @@ class RandomGamma(ImedTransform):
             return sample, metadata
 
 
+class RandomBiasField(ImedTransform):
+    """Applies a random MRI bias field artifact to the image via torchio.RandomBiasField()
+
+        Args:
+            coefficients (float): Maximum magnitude of polynomial coefficients
+            order: Order of the basis polynomial functions
+            p (float): Probability of performing the gamma contrast
+        """
+
+    def __init__(self, coefficients, order, p=0.5):
+        self.coefficients = coefficients
+        self.order = order
+        self.p = p
+
+    @multichannel_capable
+    @two_dim_compatible
+    def __call__(self, sample, metadata=None):
+        if np.random.random() < self.p:
+            # Get params
+            random_bias_field = tio.Compose([tio.RandomBiasField(coefficients=self.coefficients,
+                                                                 order=self.order,
+                                                                 p=self.p)])
+
+            # Save params
+            metadata[MetadataKW.BIAS_FIELD] = [random_bias_field]
+
+        else:
+            metadata[MetadataKW.BIAS_FIELD] = [None]
+
+        if any(metadata[MetadataKW.BIAS_FIELD]):
+            # Apply random bias field
+            data_out, history = tio_transform(x=sample, transform=random_bias_field)
+
+            # Keep data type
+            data_out = data_out.astype(sample.dtype)
+
+            # Update metadata to history
+            metadata[MetadataKW.BIAS_FIELD] = [history]
+
+            return data_out, metadata
+
+        else:
+            return sample, metadata
+
+
 def get_subdatasets_transforms(transform_params):
     """Get transformation parameters for each subdataset: training, validation and testing.
 
