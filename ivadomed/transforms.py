9e97fc87d0596be69be51f02b807f1d0222ea68c
Uzay Macar <oum2000@columbia.edu>
2021-12-24 22:47:48 +0300

    Implemented random blur augmentation.

--- ivadomed/transforms.py
+++ ivadomed/transforms.py
@@ -1101,6 +1101,44 @@ class RandomBiasField(ImedTransform):
             return sample, metadata
 
 
+class RandomBlur(ImedTransform):
+    """Applies a random blur to the image
+
+        Args:
+            sigma_range (tuple of floats): Standard deviation range for the gaussian filter
+            p (float): Probability of performing blur
+        """
+
+    def __init__(self, sigma_range, p=0.5):
+        self.sigma_range = sigma_range
+        self.p = p
+
+    @multichannel_capable
+    @two_dim_compatible
+    def __call__(self, sample, metadata=None):
+        if np.random.random() < self.p:
+            # Get params
+            sigma = np.random.uniform(self.sigma_range[0], self.sigma_range[1])
+
+            # Save params
+            metadata[MetadataKW.BLUR] = [sigma]
+
+        else:
+            metadata[MetadataKW.BLUR] = [None]
+
+        if any(metadata[MetadataKW.BLUR]):
+            # Apply random blur
+            data_out = gaussian_filter(sample, sigma)
+
+            # Keep data type
+            data_out = data_out.astype(sample.dtype)
+
+            return data_out, metadata
+
+        else:
+            return sample, metadata
+
+
 def get_subdatasets_transforms(transform_params):
     """Get transformation parameters for each subdataset: training, validation and testing.
 
