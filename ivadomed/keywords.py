0e90244ca8c7f8258dc316b65c8a4bd2a0964b9e
mariehbourget <54086142+mariehbourget@users.noreply.github.com>
2022-02-28 10:32:45 -0500

    Update microscopy following BEP release (#1025)
    * Remove float option for microscopy PixelSize
    
    * UPdate get_microscopy_pixelsize to read PixelSizeUnits metadata and
    convert pixel size in mm
    
    * Update docstrings
    
    * Add Keyword MetadataKW.PIXEL_SIZE
    
    * test_loader.py: add test_microscopy_pixelsize
    
    * Add keyword OptionKW.PIXEL_SIZE_UNITS
    
    * inference.py: Add pixel size units option to segment_volume
    
    * main.py: inference.py: Add pixel size units option for segment_volume in
    run_segment_command
    
    * Update test_microscopy_pixelsize with patch_filter_params
    
    * bids_dataframe.py: Update force index of microscopy files
    
    * Remove "microscopy" data type from config_bids.json (only "micr" is
    compliant)
    
    * download_data.py: Update link to data-testing r20220221

--- ivadomed/keywords.py
+++ ivadomed/keywords.py
@@ -140,6 +140,7 @@ class OptionKW:
     REMOVE_SMALL = "remove_small"
     OVERLAP_2D = "overlap_2D"
     PIXEL_SIZE = "pixel_size"
+    PIXEL_SIZE_UNITS = "pixel_size_units"
 
 
 @dataclass
@@ -194,6 +195,7 @@ class MetadataKW:
     INPUT_FILENAMES = "input_filenames"
     ROI_METADATA = "roi_metadata"
     PIXEL_SIZE = "PixelSize"
+    PIXEL_SIZE_UNITS = "PixelSizeUnits"
 
 
 @dataclass
