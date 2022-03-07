96ede49d17e04c03537ad477fa272332b5938ab5
etetteh <cutetetteh@gmail.com>
2022-03-07 11:34:33 -0500

    Add docstrings to function definition

--- ivadomed/random.py
+++ ivadomed/random.py
@@ -3,6 +3,13 @@ import random
 import numpy as np
 
 def set_seed(seed=6, rank=0):
+    """
+    Args:
+        seed: int to initialize random number generator (RNG)
+        rank: int specifying the GPU id for training
+    Returns:
+        seeded RNG in the specified libraries/packages.
+    """
     random.seed(seed + rank)
     np.random.seed(seed + rank)
     torch.manual_seed(seed + rank)
