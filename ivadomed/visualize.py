7760228dc17b394d8a84647f40821c5aa15b0aa3
Naga Karthik <emvnagakarthik@gmail.com>
2022-03-07 22:44:24 -0500

    renamed function to be logger agnostic, restored tensorboard instances

--- ivadomed/visualize.py
+++ ivadomed/visualize.py
@@ -129,8 +129,8 @@ def convert_labels_to_RGB(grid_img):
     return rgb_img
 
 
-def save_wandb_img(writer, epoch, dataset_type, input_samples, gt_samples, preds, is_three_dim=False):
-    """Saves input images, gt and predictions in tensorboard.
+def save_img(writer, epoch, dataset_type, input_samples, gt_samples, preds, wandb_params=None, is_three_dim=False):
+    """Saves input images, gt and predictions in tensorboard (and wandb depending upon the inputs in the config file).
 
     Args:
         writer (SummaryWriter): Tensorboard's summary writer.
@@ -174,16 +174,19 @@ def save_wandb_img(writer, epoch, dataset_type, input_samples, gt_samples, preds
         grid_img = vutils.make_grid(input_samples,
                                     normalize=True,
                                     scale_each=True)
+        writer.add_image(dataset_type + '/Input', grid_img, epoch)
         wandb.log({dataset_type+"/Input": wandb.Image(grid_img)})
 
         grid_img = vutils.make_grid(convert_labels_to_RGB(preds),
                                     normalize=True,
                                     scale_each=True)
+        writer.add_image(dataset_type + '/Predictions', grid_img, epoch)
         wandb.log({dataset_type+"/Predictions": wandb.Image(grid_img)})
 
         grid_img = vutils.make_grid(convert_labels_to_RGB(gt_samples),
                                     normalize=True,
                                     scale_each=True)
+        writer.add_image(dataset_type + '/Ground Truth', grid_img, epoch)
         wandb.log({dataset_type+"/Ground-Truth": wandb.Image(grid_img)})
 
 
