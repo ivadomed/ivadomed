a7125d5e5f3c946eefc2f9aec8b92e505d6c6244
etetteh <cutetetteh@gmail.com>
2022-03-14 12:59:20 -0400

    Remove rank argument, and refactor the function definition

--- ivadomed/random.py
+++ ivadomed/random.py
@@ -2,15 +2,12 @@ import torch
 import random
 import numpy as np
 
-def set_seed(seed=6, rank=0):
+def set_seed(seed: int = 6):
     """
     This function controls sources of randomness to aid reproducibility.
     Args:
         seed: int to initialize random number generator (RNG)
-        rank: int specifying the GPU id for training
-    Returns:
-        seeded RNG in the specified libraries/packages.
     """
-    random.seed(seed + rank)
-    np.random.seed(seed + rank)
-    torch.manual_seed(seed + rank)
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
