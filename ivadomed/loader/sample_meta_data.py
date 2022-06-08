47c2ce76ce9459c7dd615ac810371d5b6bef794d
hanna <hanna.ma@motioncorrect.com>
2022-06-08 18:29:55 +0100

    type hint for sample_meta_data.py

--- ivadomed/loader/sample_meta_data.py
+++ ivadomed/loader/sample_meta_data.py
@@ -1,3 +1,10 @@
+from __future__ import annotations
+import typing
+if typing.TYPE_CHECKING:
+    from typing import ItemsView
+    from typing import KeysView
+
+
 class SampleMetadata(object):
     """Metadata class to help update, get and set metadata values.
 
@@ -8,22 +15,22 @@ class SampleMetadata(object):
         metadata (dict): Image metadata.
     """
 
-    def __init__(self, d=None):
+    def __init__(self, d: dict = None) -> None:
         self.metadata = {} or d
 
-    def __setitem__(self, key, value):
+    def __setitem__(self, key: any, value: any) -> None:
         self.metadata[key] = value
 
-    def __getitem__(self, key):
+    def __getitem__(self, key: any) -> any:
         return self.metadata[key]
 
-    def __contains__(self, key):
+    def __contains__(self, key: any) -> bool:
         return key in self.metadata
 
-    def items(self):
+    def items(self) -> ItemsView:
         return self.metadata.items()
 
-    def _update(self, ref, list_keys):
+    def _update(self, ref: SampleMetadata, list_keys: list) -> None:
         """Update metadata keys with a reference metadata.
 
         A given list of metadata keys will be changed and given the values of the reference
@@ -37,5 +44,5 @@ class SampleMetadata(object):
             if (k not in self.metadata.keys() or not bool(self.metadata[k])) and k in ref.metadata.keys():
                 self.metadata[k] = ref.metadata[k]
 
-    def keys(self):
+    def keys(self) -> KeysView:
         return self.metadata.keys()
