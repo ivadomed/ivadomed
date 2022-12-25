f2d7806da028a3d6fde9180ce1f4f98eeb7cd336
Yang Ding <yang.ding@motioncorrect.com>
2022-12-24 20:05:42 -0500

    WIP Bids Loader POC

--- ivadomed/config/example_loader_v2_configs.py
+++ ivadomed/config/example_loader_v2_configs.py
@@ -165,6 +165,69 @@ example_1i1o_all_dataset_groups_config_json[DataloaderKW.DATASET_GROUPS][1].upda
     }
 )
 
+
+example_bids_dataset_group_config_json = {
+            DataloaderKW.DATASET_GROUP_LABEL: "BIDSDataSetGroup1",
+            DataloaderKW.TYPE: "BIDS",
+            DataloaderKW.PATH_DATA: path_mock_data,
+            DataloaderKW.EXPECTED_INPUT: 2,
+            DataloaderKW.EXPECTED_GT: 1,
+            DataloaderKW.TRAINING: {
+                    DataloaderKW.IMAGE_GROUND_TRUTH: [
+                        [["sub-01/ses-01/anat/sub-01_ses-01_flip-1_mt-off_MTS.nii",
+                          "sub-01/ses-01/anat/sub-01_ses-01_flip-1_mt-on_MTS.nii"],
+                         ["derivatives/labels/sub-01/ses-01/anat/sub-01_ses-01_mt-off_MTS_lesion-manual-rater1.nii"]],
+                        [["sub-01/ses-02/anat/sub-01_ses-02_flip-1_mt-off_MTS.nii",
+                          "sub-01/ses-02/anat/sub-01_ses-02_flip-1_mt-on_MTS.nii"],
+                         ["derivatives/labels/sub-01/ses-02/anat/sub-01_ses-02_mt-off_MTS_lesion-manual-rater1.nii"]],
+                        [["sub-01/ses-03/anat/sub-01_ses-03_flip-1_mt-off_MTS.nii",
+                          "sub-01/ses-03/anat/sub-01_ses-03_flip-1_mt-on_MTS.nii",
+                          "sub-01/ses-03/anat/sub-01_ses-03_flip-1_mt-on_MTS.nii"],
+                         ["derivatives/labels/sub-01/ses-03/anat/sub-01_ses-03_mt-off_MTS_lesion-manual-rater1.nii"]],
+                        [["sub-01/ses-04/anat/sub-01_ses-04_flip-1_mt-off_MTS.nii", ],
+                         ["derivatives/labels/sub-01/ses-04/anat/sub-01_ses-04_mt-off_MTS_lesion-manual-rater1.nii"]],
+                    ],
+                    DataloaderKW.MISSING_FILES_HANDLE: FileMissingHandleKW.SKIP,
+                    DataloaderKW.EXCESSIVE_FILES_HANDLE: FileExcessiveHandleKW.USE_FIRST_AND_WARN,
+            },
+            DataloaderKW.VALIDATION: {
+                    DataloaderKW.IMAGE_GROUND_TRUTH: [
+                        [["sub-02/ses-01/anat/sub-02_ses-01_flip-1_mt-off_MTS.nii",
+                          "sub-02/ses-01/anat/sub-02_ses-01_flip-1_mt-on_MTS.nii"],
+                         ["derivatives/labels/sub-02/ses-01/anat/sub-02_ses-01_mt-off_MTS_lesion-manual-rater1.nii"]],
+                        [["sub-02/ses-02/anat/sub-02_ses-02_flip-1_mt-off_MTS.nii",
+                          "sub-02/ses-02/anat/sub-02_ses-02_flip-1_mt-on_MTS.nii"],
+                         ["derivatives/labels/sub-02/ses-02/anat/sub-02_ses-02_mt-off_MTS_lesion-manual-rater1.nii"]],
+                        [["sub-02/ses-03/anat/sub-02_ses-03_flip-1_mt-off_MTS.nii",
+                          "sub-02/ses-03/anat/sub-02_ses-03_flip-1_mt-on_MTS.nii",
+                          "sub-02/ses-03/anat/sub-02_ses-03_flip-1_mt-on_MTS.nii"],
+                         ["derivatives/labels/sub-02/ses-03/anat/sub-02_ses-03_mt-off_MTS_lesion-manual-rater1.nii"]],
+                        [["sub-02/ses-04/anat/sub-02_ses-04_flip-1_mt-off_MTS.nii", ],
+                         ["derivatives/labels/sub-02/ses-04/anat/sub-02_ses-04_mt-off_MTS_lesion-manual-rater1.nii"]],
+                    ],
+                    DataloaderKW.MISSING_FILES_HANDLE: FileMissingHandleKW.SKIP,
+                    DataloaderKW.EXCESSIVE_FILES_HANDLE: FileExcessiveHandleKW.USE_FIRST_AND_WARN,
+            },
+            DataloaderKW.TEST: {
+                    DataloaderKW.IMAGE_GROUND_TRUTH:  [
+                        [["sub-03/ses-01/anat/sub-03_ses-01_flip-1_mt-off_MTS.nii",
+                          "sub-03/ses-01/anat/sub-03_ses-01_flip-1_mt-on_MTS.nii"],
+                         ["derivatives/labels/sub-03/ses-01/anat/sub-03_ses-01_mt-off_MTS_lesion-manual-rater1.nii"]],
+                        [["sub-03/ses-02/anat/sub-03_ses-02_flip-1_mt-off_MTS.nii",
+                          "sub-03/ses-02/anat/sub-03_ses-02_flip-1_mt-on_MTS.nii"],
+                         ["derivatives/labels/sub-03/ses-02/anat/sub-03_ses-02_mt-off_MTS_lesion-manual-rater1.nii"]],
+                        [["sub-03/ses-03/anat/sub-03_ses-03_flip-1_mt-off_MTS.nii",
+                          "sub-03/ses-03/anat/sub-03_ses-03_flip-1_mt-on_MTS.nii",
+                          "sub-03/ses-03/anat/sub-03_ses-03_flip-1_mt-on_MTS.nii"],
+                         ["derivatives/labels/sub-03/ses-03/anat/sub-03_ses-03_mt-off_MTS_lesion-manual-rater1.nii"]],
+                        [["sub-03/ses-04/anat/sub-03_ses-04_flip-1_mt-off_MTS.nii", ],
+                         ["derivatives/labels/sub-03/ses-04/anat/sub-03_ses-04_mt-off_MTS_lesion-manual-rater1.nii"]],
+                    ],
+                    DataloaderKW.MISSING_FILES_HANDLE: FileMissingHandleKW.SKIP,
+                    DataloaderKW.EXCESSIVE_FILES_HANDLE: FileExcessiveHandleKW.USE_FIRST_AND_WARN,
+            },
+}
+
 if __name__=="__main__":
 
     import pprint
