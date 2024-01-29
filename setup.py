1cb954a1accfe775a78e6b52a72f9269cfe0e700
Mathieu Boudreau <emb6150@gmail.com>
2024-01-29 13:02:57 -0400

    Try doc fix

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ extra_requirements = {
     'docs': [
         # pin sphinx to match what RTD uses:
         # https://github.com/readthedocs/readthedocs.org/blob/ecac31de54bbb2c100f933e86eb22b0f4389ba84/requirements/pip.txt#L16
-        'sphinx',
+        'sphinx>-5.0',
         'sphinx_rtd_theme',
         'sphinx-tabs==3.2.0',
         'sphinx-toolbox==2.15.2',
